aec4b481f0048e5688ea5cf252b8a1e3bf2ea132
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-10 09:26:10 +0200

    adding profile

---
+++ culturefeed_ui/culturefeed-ui-profile.tpl.php
@@ -0,0 +1,26 @@
+<dl>
+  
+  <dt>Nick</dt>
+  <dd><?php print $nick ?></dd>
+  
+  <?php if ($name) : ?>
+    <dt>Naam</dt>
+    <dd><?php print $name ?></dd>
+  <?php endif; ?>
+  
+  <?php if ($gender) : ?>
+    <dt>Geslacht</dt>
+    <dd><?php print $gender ?></dd>
+  <?php endif; ?>
+  
+  <?php if ($dob) : ?>
+    <dt>Geboortedatum</dt>
+    <dd><?php print $dob?></dd>
+  <?php endif; ?>
+  
+  <?php if ($bio) : ?>
+    <dt>Bio</dt>
+    <dd><?php print $bio ?></dd>
+  <?php endif; ?>
+  
+</dl>
