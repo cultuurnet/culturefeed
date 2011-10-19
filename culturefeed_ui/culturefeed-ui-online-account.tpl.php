5bb547bd1a76fe04a17a7734c4dd86e4d70361ca
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-19 11:19:46 +0200

    culturefeed-ui-online-account.tpl.php

---
+++ culturefeed_ui/culturefeed-ui-online-account.tpl.php
@@ -0,0 +1,5 @@
+<div class="name"><?php print $name ?></div>
+<div class="type"><?php print $type ?></div>
+<div class="publish"><?php print $publish_checkbox ?></div>
+<div class="delete"><?php print $delete_link ?></div>
+<div class="connect"><?php print $connect_link ?></div>
