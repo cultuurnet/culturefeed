bb700c239861c67238ba70e7f670bcd769230474
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-02 16:48:31 +0100

    make likes work on all content types

--- lib/CultureFeed/CultureFeed/HttpException.php
+++ lib/CultureFeed/CultureFeed/HttpException.php
@@ -4,7 +4,6 @@ class CultureFeed_HttpException extends Exception {
   public $body;
 
   function __construct($body, $code) {
-    parent::__construct('The reponse for the HTTP request was not 200.', $code);
-    $this->body = $body;
+    parent::__construct('The reponse for the HTTP request was not 200. ' . $body, $code);
   }
 }
