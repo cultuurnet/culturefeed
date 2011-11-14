61502dff530fc508d89f75f57fc89b4edc2f327e
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-14 14:38:07 +0100

    more work on recommendations

---
+++ culturefeed_ui/culturefeed-ui-event-recommendation.tpl.php
@@ -0,0 +1,9 @@
+<h<?php print $heading_level ?><?php print $title_attributes; ?>><?php print $title ?></h<?php print $heading_level ?>>
+
+<p><?php print $date ?></p>
+
+<p><?php print $location ?></p>
+
+<p><?php print $more ?></p>
+
+<p><?php print $id ?></p>
