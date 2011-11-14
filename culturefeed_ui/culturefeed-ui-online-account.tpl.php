8481cc8c78a210bcc5678628c2cb1ab2c2a2fff6
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-14 16:56:38 +0100

    fixing recent activities

--- culturefeed_ui/culturefeed-ui-online-account.tpl.php
+++ culturefeed_ui/culturefeed-ui-online-account.tpl.php
@@ -1,7 +1,7 @@
-<div class="name"><?php print $name ?></div>
-<div class="nick"><?php print $nick ?></div>
+<div class="type <?php print $type ?>"><span><?php print $type ?></span></div>
 <div class="depiction"><?php print $picture ?></div>
-<div class="type"><?php print $type ?></div>
+<div class="name"><?php print $name ?></div> 
+<div class="nick"><?php print $nick ?></div>
 <div class="publish"><?php print $publish_checkbox ?></div>
 <div class="delete"><?php print $delete_link ?></div>
 <div class="connect"><?php print $connect_link ?></div>
