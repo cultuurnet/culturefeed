7dd32b6ed4baba0b02276df314694e8027625967
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-15 19:20:08 +0100

    fixes

--- culturefeed_ui/culturefeed-ui-event-recommendation.tpl.php
+++ culturefeed_ui/culturefeed-ui-event-recommendation.tpl.php
@@ -1,15 +1,19 @@
 <h<?php print $heading_level ?><?php print $title_attributes; ?>><?php print $title ?></h<?php print $heading_level ?>>
 
-<div class="when"><?php print $date ?></div>
-
 <div class="where"><?php print $location ?></div>
 
-<div class="more-link"><?php print $more ?></div>
+<div class="when"><?php print $date ?></div>
 
-<?php if ($like_link) : ?>
-  <p><?php print $like_link ?></p>
-<?php endif; ?>
+<div class="more"><?php print $more ?></div>
 
-<?php if ($remove_link) : ?>
-  <p><?php print $remove_link ?></p>
-<?php endif; ?>
+<div class="actions">
+  <?php if ($like_link) : ?>
+    <div class="like"><?php print $like_link ?></div>
+  <?php endif; ?>
+  
+  <?php if ($remove_link) : ?>
+    <div class="remove"><?php print $remove_link ?></div>
+  <?php endif; ?>
+</div>
+
+<div class="clearfix"></div>
