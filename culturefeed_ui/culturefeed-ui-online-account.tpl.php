c6148469945d16741bf6f2064baab5ce0f7126e7
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-16 15:58:06 +0100

    ajax for online account

--- culturefeed_ui/culturefeed-ui-online-account.tpl.php
+++ culturefeed_ui/culturefeed-ui-online-account.tpl.php
@@ -2,6 +2,6 @@
 <div class="depiction"><?php print $picture ?></div>
 <div class="name"><?php print $name ?></div>
 <div class="nick"><?php print $nick ?></div>
-<div class="publish"><?php print $publish_checkbox ?></div>
+<div class="publish"><?php print $publish_link ?></div>
 <div class="delete"><?php print $delete_link ?></div>
 <div class="connect"><?php print $connect_link ?></div>
