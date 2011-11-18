1f0650617be69e681770876989565e04fdb7b7ef
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-18 15:29:14 +0100

    recommendations with ajax

--- culturefeed_ui/js/culturefeed_ui.js
+++ culturefeed_ui/js/culturefeed_ui.js
@@ -1,14 +1,11 @@
 (function ($) {
-
-Drupal.behaviors.cultureFeedUIRelatedEvents = {
-  attach: function (context, settings) {
-    if (Drupal.settings.culturefeed_ui.related_events) {
-      var url = Drupal.settings.culturefeed_ui.related_events;
-      $.get(url, function(data) {
-        $('#related-events').html(data);
-      });
+  
+  $(document).ready(function() {
+    for (var base in Drupal.settings.ajax) {
+      if (Drupal.settings.ajax[base].event == 'ajaxload') {
+        $(Drupal.settings.ajax[base].selector).trigger('ajaxload');
+      }
     }
-  }
-};
+  });
 
 })(jQuery);
