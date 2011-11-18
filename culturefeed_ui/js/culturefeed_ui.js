67cd1e2721090f96bb91dcbda7e30020076a6d13
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-18 15:57:03 +0100

    overriding Drupal.ajax.prototype.error

--- culturefeed_ui/js/culturefeed_ui.js
+++ culturefeed_ui/js/culturefeed_ui.js
@@ -1,3 +1,26 @@
+/**
+ * Handler for the form redirection error.
+ */
+Drupal.ajax.prototype.error = function (response, uri) {
+  // alert(Drupal.ajaxError(response, uri)); // we don't want Drupal's default behavior to show alerts!
+  // Remove the progress element.
+  if (this.progress.element) {
+    $(this.progress.element).remove();
+  }
+  if (this.progress.object) {
+    this.progress.object.stopMonitoring();
+  }
+  // Undo hide.
+  $(this.wrapper).show();
+  // Re-enable the element.
+  $(this.element).removeClass('progress-disabled').removeAttr('disabled');
+  // Reattach behaviors, if they were detached in beforeSerialize().
+  if (this.form) {
+    var settings = response.settings || this.settings || Drupal.settings;
+    Drupal.attachBehaviors(this.form, settings);
+  }
+};
+
 (function ($) {
   
   $(document).ready(function() {
