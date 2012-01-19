b1173a7b8c07e2a6630cf2a215097950c88ef0f7
Laurens Vandeput <laurens@syncleon.com>
2012-01-19 12:32:15 +0100

    CULPAS-1

---
+++ lib/CultureFeed/CultureFeed/Uitpas.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ *
+ */
+interface CultureFeed_Uitpas {
+
+}
---
+++ lib/CultureFeed/CultureFeed/Uitpas/Default.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ */
+class CultureFeed_Uitpas_Default implements CultureFeed_Uitpas {
+
+  /**
+   *
+   * CultureFeed object to make CultureFeed core requests.
+   * @var ICultureFeed
+   */
+  protected $culturefeed;
+
+  /**
+   * OAuth request object to do the request.
+   *
+   * @var CultureFeed_OAuthClient
+   */
+  protected $oauth_client;
+
+  /**
+   *
+   * Constructor for a new UitPas_Default instance
+   * @param ICultureFeed $culturefeed
+   */
+  public function __construct(ICultureFeed $culturefeed) {
+    $this->culturefeed = $culturefeed;
+    $this->oauth_client = $culturefeed->getClient();
+  }
+
+}
