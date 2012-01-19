8b5e574bfc7b0bff64ff5c29c143e1850c6530b0
Laurens Vandeput <laurens@syncleon.com>
2012-01-19 15:03:04 +0100

    CULWID-1
    Method signatures UitPas

--- lib/CultureFeed/CultureFeed/Uitpas.php
+++ lib/CultureFeed/CultureFeed/Uitpas.php
@@ -5,4 +5,195 @@
  */
 interface CultureFeed_Uitpas {
 
+  /**
+   * Get the distribution keys for an organizer.
+   *
+   * @param string $cdbid The CDBID of the organizer
+   */
+  public function getDistributionKeysForOrganizer($cdbid);
+
+  /**
+   * Register a set of distribution keys for an organizer. The entire set (including existing)
+   * of distribution keys must be provided.
+   *
+   * @param string $cdbid The CDBID of the organizer
+   * @param unknown_type $distribution_keys The identification of the distribution key
+   */
+  public function registerDistributionKeysForOrganizer($cdbid, $distribution_keys);
+
+  /**
+   * Get the price of the UitPas.
+   */
+  public function getPrice();
+
+  /**
+   * Create a new UitPas passholder.
+   *
+   * @param CultureFeed_Uitpas_Passholder $passholder The new passholder
+   */
+  public function createPassholder(CultureFeed_Uitpas_Passholder $passholder);
+
+  /**
+   * Create a new membership for a UitPas passholder.
+   *
+   * @param string $id The user ID of the passholder
+   * @param string $organization The name of the organization
+   * @param DateTime $end_date The membership's organization end date
+   */
+  public function createMembershipForPassholder($id, $organization, $end_date);
+
+  /**
+   * Get a passholder based on the UitPas number.
+   *
+   * @param string $uitpas_number The UitPas number
+   */
+  public function getPassholder($uitpas_number);
+
+  /**
+   * Search for passholders.
+   *
+   * @param CultureFeed_Uitpas_SearchPassHoldersQuery $query The query
+   */
+  public function searchPassholders(CultureFeed_Uitpas_SearchPassHoldersQuery $query);
+
+  /**
+   * Get the welcome advantages for a passholder.
+   *
+   * @param string $uitpas_number The UitPas number
+   */
+  public function getWelcomeAdvantagesForPassholder($uitpas_number);
+
+  /**
+   * Check in a passholder.
+   *
+   * Provide either a UitPas number or chip number. You cannot provide both.
+   *
+   * @param string $cdbid The CDBID of the event
+   * @param string $uitpas_number The UitPas number
+   * @param string $chip_number NFC card chip number
+   */
+  public function checkinPassholder($cdbid, $uitpas_number, $chip_number);
+
+  /**
+   * Cash in a welcome advantage.
+   *
+   * @param string $uitpas_number The UitPas number
+   * @param int $welcome_advantage_id Identification welcome advantage
+   */
+  public function cashInWelcomeAdvantage($uitpas_number, $welcome_advantage_id);
+
+  /**
+   * Get the redeem options
+   *
+   * @param CultureFeed_Uitpas_SearchPointsPromotionsOptionsQuery $query The query
+   */
+  public function getPointsPromotions(CultureFeed_Uitpas_SearchPointsPromotionsOptionsQuery $query);
+
+  /**
+   * Cash in promotion points for a UitPas.
+   *
+   * @param string $uitpas_number The UitPas number
+   * @param int $points_promotion_id The identification of the redeem option
+   * @param string $bar The name of the UitPas bar
+   */
+  public function cashInPointsPromotion($uitpas_number, $points_promotion_id, $bar);
+
+  /**
+   * Upload a picture for a given passholder.
+   *
+   * @param string $id The user ID of the passholder
+   * @param string $file_data The binary data of the picture
+   */
+  public function uploadPicture($id, $file_data);
+
+  /**
+   * Update a passholder.
+   *
+   * @param CultureFeed_Uitpas_Passholder $passholder The passholder to update.
+   * 		The passholder is identified by ID. Only fields that are set will be updated.
+   */
+  public function updatePassholder(CultureFeed_Uitpas_Passholder $passholder);
+
+  /**
+   * Block a UitPas.
+   *
+   * @param string $uitpas_number The UitPas number
+   */
+  public function blockUitpas($uitpas_number);
+
+  /**
+   * Search for welcome advantages.
+
+   * @param CultureFeed_Uitpas_SearchWelcomeAdvantagesQuery $query The query
+   */
+  public function searchWelcomeAdvantages(CultureFeed_Uitpas_SearchWelcomeAdvantagesQuery $query);
+
+  /**
+   * Get a passholder based on the UitPas chip number.
+   *
+   * @param string $chip_number The chipnumber of the UitPas
+   */
+  public function getPassholderForChipNumber($chip_number);
+
+  /**
+   * Get the events for a given passholder.
+   *
+   * @param string $uitpas_number The UitPas number
+   * @param DateTime $date_from Start date
+   * @param DateTime $date_to End date
+   */
+  public function getEventsForPassholder($uitpas_number, $date_from, $date_to);
+
+  /**
+   * Register a ticket sale for a passholder
+   *
+   * @param string $uitpas_number The UitPas number
+   * @param string $cdbid The event CDBID
+   */
+  public function registerTicketSale($uitpas_number, $cdbid);
+
+  /**
+   * Cancel a ticket sale for a passholder
+   *
+   * @param string $uitpas_number The UitPas number
+   * @param string $cdbid The event CDBID
+   */
+  public function cancelTicketSale($uitpas_number, $cdbid);
+
+  /**
+   * Get the accumulated points of a passholder.
+   *
+   * @param CultureFeed_Uitpas_AccumulatedPointsQuery $query The query
+   */
+  public function getAccumulatedPoints(CultureFeed_Uitpas_AccumulatedPointsQuery $query);
+
+  /**
+   * Search for Uitpas events
+   *
+   * @param CultureFeed_Uitpas_SearchEventsQuery $query The query
+   */
+  public function searchEvents(CultureFeed_Uitpas_SearchEventsQuery $query);
+
+  /**
+   * Search for point of sales
+   *
+   * @param CultureFeed_Uitpas_SearchPointOfSalesQuery $query The query
+   */
+  public function searchPointOfSales(CultureFeed_Uitpas_SearchPointOfSalesQuery $query);
+
+  /**
+   * Add a member to a bar.
+   *
+   * @param int $id The bar ID
+   * @param string $uid The Culturefeed user ID
+   */
+  public function addMemberToBar($id, $uid);
+
+  /**
+   * Search for bars for a given member
+   *
+   * @param string $uid The Culturefeed user ID
+   */
+  public function searchBarsForMember($uid);
+
 }
