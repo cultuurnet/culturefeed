af89c42886444e13864925faf701c3f03b89881f
Hans Langouche <hans.langouche@gmail.com>
2012-05-04 01:50:30 +0200

    CULPAS-193 pagina's gebruikersprofiel

--- uitpas_ui/theme/uitpas-ui-user-profile-activities.tpl.php
+++ uitpas_ui/theme/uitpas-ui-user-profile-activities.tpl.php
@@ -1,2 +1,4 @@
-<div class="uitpas_ui_user_profile_advantages">
+<div class="uitpas_ui_user_profile_activities">
+  <h2><?php print $title; ?></h2>
+  <?php print $activities; ?>
 </div>
--- uitpas_ui/theme/uitpas-ui-user-profile-details.tpl.php
+++ uitpas_ui/theme/uitpas-ui-user-profile-details.tpl.php
@@ -1,2 +1,33 @@
 <div class="uitpas_ui_user_profile_details">
+  <h2><?php print $title; ?></h2>
+  <div class="uitpas_ui_user_profile_details_uitpas">
+    <div><?php print $active_since; ?></div>
+    <div><?php print $uitpas_number; ?></div>
+  </div>
+  <div class="uitpas_ui_user_profile_details_data">
+    <div class="uitpas_ui_user_profile_details_intro"><?php print $intro; ?></div>
+    <?php if ($image): ?>
+      <div class="uitpas_ui_user_profile_details_image"><?php print $image; ?></div>
+    <?php endif; ?>
+    <div><?php print $national_identification_number; ?></div>
+    <div><?php print $first_name; ?></div>
+    <div><?php print $last_name; ?></div>
+    <div><?php print $dob; ?></div>
+    <div><?php print $pob; ?></div>
+    <div><?php print $gender; ?></div>
+    <div><?php print $nationality; ?></div>
+    <div><?php print $street; ?></div>
+    <div><?php print $nr; ?></div>
+    <div><?php print $bus; ?></div>
+    <div><?php print $zip; ?></div>
+    <div><?php print $city; ?></div>
+    <div><?php print $tel; ?></div>
+    <div><?php print $mobile; ?></div>
+    <div><?php print $email; ?></div>
+    <div class="uitpas_ui_user_profile_details_status">
+      <h3><?php print $status_title; ?></h3>
+      <div><?php print $status_valid_till; ?></div>
+      <div><?php print $ocmw_valid_till; ?></div>
+    </div>
+  </div>
 </div>
