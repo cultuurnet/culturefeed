45976cecb913b01ee52ff11719ef93b824e8d5b0
Hans Langouche <hans.langouche@gmail.com>
2012-05-04 00:08:30 +0200

    CULPAS-199 pagina mijn voordelen van gebruikersprofiel (+ some cleanup)

--- uitpas_ui/theme/uitpas-ui-user-profile-advantages.tpl.php
+++ uitpas_ui/theme/uitpas-ui-user-profile-advantages.tpl.php
@@ -1,2 +1,8 @@
 <div class="uitpas_ui_user_profile_advantages">
+  <h2><?php print $title; ?></h2>
+  <div class="uitpas_ui_user_profile_advantages_promotions"><?php print $promotions; ?></div>
+  <div class="uitpas_ui_user_profile_advantages_advantages"><?php print $advantages; ?></div>
+  <div class="uitpas_ui_user_profile_advantages_cashed_in_promotions_advantages">
+    <?php print $cashed_in_promotions_advantages; ?>
+  </div>
 </div>
