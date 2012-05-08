1d1bd4335240942b76644e4ee335215c571a609c
Hans Langouche <hans.langouche@gmail.com>
2012-05-08 10:42:41 +0200

    CULPAS-189 pagina uitpas aanvragen

---
+++ uitpas_ui/theme/uitpas-ui-register-where.tpl.php
@@ -0,0 +1,4 @@
+<div class="uitpas_ui_register_where">
+  <div class="uitpas_ui_register_where_pos"><?php print $pos; ?></div>
+  <div class="uitpas_ui_register_where_info"><?php print $info; ?></div>
+</div>
