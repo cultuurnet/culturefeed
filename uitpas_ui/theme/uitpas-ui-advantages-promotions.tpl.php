de3b8c20e4469bb35e111fa7353463202a40c8e1
Hans Langouche <hans@hnln.be>
2012-05-31 15:29:21 +0200

    CULPAS-267 updates deployen naar site repository

--- uitpas_ui/theme/uitpas-ui-advantages-promotions.tpl.php
+++ uitpas_ui/theme/uitpas-ui-advantages-promotions.tpl.php
@@ -1,5 +1,5 @@
 <div class="uitpas_ui_advantages_promotions">
+  <div class="uitpas_ui_advantages_promotions_info"><?php print $info; ?></div>
   <div class="uitpas_ui_advantages_promotions_advantages"><?php print $advantages; ?></div>
   <div class="uitpas_ui_advantages_promotions_promotions"><?php print $promotions; ?></div>
-  <div class="uitpas_ui_advantages_promotions_info"><?php print $info; ?></div>
 </div>
