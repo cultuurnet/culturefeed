d6a66ae9204b7173ef276ca42af59139d3d62b5e
zuuperman <nils.destoop@gmail.com>
2012-10-05 10:29:36 +0200

    Merged cultuurnet_culturefeed with culturefeed from davy

---
+++ culturefeed_mailing/culturefeed-mailing-optin.tpl.php
@@ -0,0 +1,3 @@
+<?php if ($newsletter_optin_toggle): ?>
+  <?php print $newsletter_optin_toggle; ?>
+<?php endif;?>
--- culturefeed_ui/culturefeed-ui-profile-box.tpl.php
+++ culturefeed_ui/culturefeed-ui-profile-box.tpl.php
@@ -18,6 +18,7 @@
     <ul>
       <li><?php print $link_login ?></li>
       <li><?php print $link_register ?></li>
+      <li><?php print $link_login_facebook ?></li>
     </ul>
   </div>
 
