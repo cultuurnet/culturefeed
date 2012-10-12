6612a0262ee24a1cda4895df0b1e0ac3e9df1c54
Robert Brown <robert@statik.be>
2012-10-12 15:01:47 +0200

    CULPAS-360 Small addition to bring in the groups

--- lib/CultureFeed/CultureFeed/Uitpas/Counter/Employee.php
+++ lib/CultureFeed/CultureFeed/Uitpas/Counter/Employee.php
@@ -29,7 +29,7 @@ class CultureFeed_Uitpas_Counter_Employee extends CultureFeed_Uitpas_ValueObject
    * @var bool
    */
   public $role;
-  
+
   /**
    * The permissions of the member in the counter
    *
@@ -37,16 +37,30 @@ class CultureFeed_Uitpas_Counter_Employee extends CultureFeed_Uitpas_ValueObject
    */
   public $permissions = array();
 
+  /**
+   * The groups of the memeber in the counter.
+   *
+   *  @var array
+   */
+  public $groups;
+
+
   public static function createFromXML(CultureFeed_SimpleXMLElement $object) {
     $counter = new CultureFeed_Uitpas_Counter_Employee();
     $counter->id = $object->xpath_str('id');
     $counter->consumerKey = $object->xpath_str('consumerKey');
     $counter->name = $object->xpath_str('name');
     $counter->role = $object->xpath_str('role');
-    
+
     foreach ($object->xpath('permissions/permission') as $permission) {
       $counter->permissions[] = (string) $permission;
     }
+
+    foreach ($object->xpath('groups/group') as $group) {
+      $counter->groups[] = (string) $group;
+    }
+
+
 
     return $counter;
   }
