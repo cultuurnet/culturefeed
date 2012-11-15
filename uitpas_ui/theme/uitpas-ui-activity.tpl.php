4e3dace645313372e9571ef894f14e2eb092642e
Luk Dens <luk.dens@cultuurnet.be>
2012-11-15 15:52:23 +0100

    always show location and organiser link

--- uitpas_ui/theme/uitpas-ui-activity.tpl.php
+++ uitpas_ui/theme/uitpas-ui-activity.tpl.php
@@ -7,7 +7,10 @@
     <div class="uitpas_ui_activity_details_general_shortdescription"><?php print $shortdescription; ?></div>
     <div class="uitpas_ui_activity_details_general_when"><?php print $when; ?></div>
     <div class="uitpas_ui_activity_details_general_where"><?php print $where; ?></div>
-    <div class="uitpas_ui_activity_details_general_price"><?php print $price; ?></div>
+    <div class="uitpas_ui_activity_details_general_price"><?php print $price; ?></div>    
+    <?php if ($organisation): ?>
+      <div class="uitpas_ui_activity_details_general_organisation"><?php print $organisation; ?></div>
+    <?php endif; ?>
     <div class="uitpas_ui_activity_details_general_links"><?php print $links; ?></div>
   </div>
   <?php if ($image): ?>
