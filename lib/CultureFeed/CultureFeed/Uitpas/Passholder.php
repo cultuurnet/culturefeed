ced77b443963ca3c72a743a7ded2ec0f020bb3e7
cultuurnet <sven.houtmeyers@cultuurnet.be>
2012-11-26 18:11:58 +0100

    UITPAS-361

--- lib/CultureFeed/CultureFeed/Uitpas/Passholder.php
+++ lib/CultureFeed/CultureFeed/Uitpas/Passholder.php
@@ -298,9 +298,11 @@ class CultureFeed_Uitpas_Passholder extends CultureFeed_Uitpas_ValueObject {
     $passholder->schoolConsumerKey = $object->xpath_str('schoolConsumerKey');
     $passholder->picture = $object->xpath_str('picture');
 
-    foreach ($object->xpath('memberships') as $membership) {
+    foreach ($object->xpath('memberships/membership') as $membership) {
       $memberships[] = CultureFeed_Uitpas_Passholder_Membership::createFromXML($membership);
     }
+    
+    $passholder->memberships = $memberships;
 
     return $passholder;
   }
--- lib/CultureFeed/CultureFeed/Uitpas/Passholder/Membership.php
+++ lib/CultureFeed/CultureFeed/Uitpas/Passholder/Membership.php
@@ -3,11 +3,18 @@
 class CultureFeed_Uitpas_Passholder_Membership extends CultureFeed_Uitpas_ValueObject {
 
   /**
-   * The association the passholder is linked
+   * The id of the association the passholder is linked
    *
    * @var string
    */
-  public $association;
+  public $associationId;
+  
+  /**
+   * The name of the association the passholder is linked
+   *
+   * @var string
+   */
+  public $name; 
 
   /**
    * The membership's organization end date. (Required)
@@ -16,9 +23,18 @@ class CultureFeed_Uitpas_Passholder_Membership extends CultureFeed_Uitpas_ValueO
    */
   public $endDate;
   
+  protected function manipulatePostData(&$data) {
+    
+    if (isset($data['endDate'])) {
+      $data['endDate'] = date('Y-m-d', $data['endDate']);
+    }
+  }
+  
   public static function createFromXML(CultureFeed_SimpleXMLElement $object) {
+
     $membership = new CultureFeed_Uitpas_Passholder_Membership();
-    $membership->association = $object->xpath_str('association');
+    $membership->associationId = $object->xpath_str('association/id');
+    $membership->name = $object->xpath_str('association/name');
     $membership->endDate = $object->xpath_time('endDate');
 
     return $membership;
