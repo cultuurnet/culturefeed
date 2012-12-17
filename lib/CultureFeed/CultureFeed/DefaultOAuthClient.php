96a5f26b4dc27e78c09de3c5ac6b159aae91d26e
cultuurnet <sven.houtmeyers@cultuurnet.be>
2012-12-17 12:45:30 +0100

    adding url and post body to CultureFeed_HttpException

--- lib/CultureFeed/CultureFeed/DefaultOAuthClient.php
+++ lib/CultureFeed/CultureFeed/DefaultOAuthClient.php
@@ -332,23 +332,10 @@ class CultureFeed_DefaultOAuthClient implements CultureFeed_OAuthClient {
     // Do the request.
     $response = $this->http_client->request($url, $http_headers, $method, $post_data);
 
-    //dpm( $url , 'URL called');
-    //dpm( $post_data , 'Post Data: ');
-    //dpm( $response , 'Response: ');
-
-
     // In case the HTTP response status is not 200, we consider this an error.
     // In case we can parse a code and message from the response, we throw a CultureFeed_Exception.
     // In case we can't parse a code and message, we throw a CultureFeed_HTTPException.
     if ($response->code != 200) {
-
-
-      watchdog( 'oAuth' , 'URL CALLED' , $url);
-      watchdog( 'oAuth', 'POST DATA', $post_data);
-      watchdog( 'oAuth', 'Response', $response);
-
-
-
 
       try {
         $xml = new CultureFeed_SimpleXMLElement($response->response);
@@ -361,7 +348,7 @@ class CultureFeed_DefaultOAuthClient implements CultureFeed_OAuthClient {
         $message = $xml->xpath_str('/response/message');
         throw new CultureFeed_Exception($message, $code);
       }
-      throw new CultureFeed_HttpException($response->response, $response->code);
+      throw new CultureFeed_HttpException($response->response . 'URL CALLED: ' . $url . 'POST DATA: <pre>' . $post_data . '</pre>', $response->code);
     }
 
     // In case the HTTP response status is 200, we return the response.
