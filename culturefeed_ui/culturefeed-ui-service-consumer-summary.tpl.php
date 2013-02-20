2388e9572c246a1d0986c6feec27151ecc857b4e
Veerle Hanssens <veerlehanssens@Veerles-MacBook-Pro.local>
2013-02-20 12:08:27 +0100

    update - remove description service consumer accounts (CFUIV-317)

--- culturefeed_ui/culturefeed-ui-service-consumer-summary.tpl.php
+++ culturefeed_ui/culturefeed-ui-service-consumer-summary.tpl.php
@@ -2,7 +2,6 @@
   <h2><?php print $name ?></h2>
   <div class="teaser-sc-info">
     <div class="image"><?php print $logo ?></div>
-    <div class="intro"><?php print $description ?></div>
     <div class="date"><?php print $creation_date ?></div>
     <div class="action-links"><?php print $revoke_link ?></div>
   </div>
