f191e50539541da78cecbf6a7c9c71de65781daf
Kristof Coomans <kristof@coomans.eu>
2013-02-25 23:13:46 +0100

    fixed CultureFeed_Uitpas_EventsRegisterTest case

--- lib/CultureFeed/test/uitpas/CultureFeed_Uitpas_EventRegisterTest.php
+++ lib/CultureFeed/test/uitpas/CultureFeed_Uitpas_EventRegisterTest.php
@@ -13,7 +13,22 @@ class CultureFeed_Uitpas_EventsRegisterTest extends PHPUnit_Framework_TestCase {
    */
   public function testRegisterEvent() {
 
+    $response = <<<XML
+<?xml version="1.0" encoding="utf-8" ?>
+<response>
+<code>1</code>
+<message>foo</message>
+</response>
+XML;
+
+    /* @var $oauth_client_stub PHPUnit_Framework_MockObject_MockObject */
     $oauth_client_stub = $this->getMock('CultureFeed_OAuthClient');
+    $oauth_client_stub
+      ->expects($this->once())
+      ->method('consumerPostAsXml')
+      ->with($this->equalTo('/uitpas/cultureevent/register'))
+      ->will($this->returnValue($response));
+
     $event_xml_str = file_get_contents(dirname(__FILE__) . self::EVENTXML);
     $event_xml_obj = new CultureFeed_SimpleXMLElement($event_xml_str);
 
@@ -26,6 +41,11 @@ class CultureFeed_Uitpas_EventsRegisterTest extends PHPUnit_Framework_TestCase {
     $cf = new CultureFeed($oauth_client_stub);
 
     $response = $cf->uitpas()->registerEvent($event);
+
+    $this->assertInstanceOf('\CultureFeed_Uitpas_Response', $response);
+
+    $this->assertEquals(1, $response->code);
+    $this->assertEquals('foo', $response->message);
   }
 
 }
