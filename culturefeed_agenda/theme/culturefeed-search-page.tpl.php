bfc9c9919d96ca61a2d7d1c953eb30bb21a28185
Jochen Stals <jochen.stals@wunderkraut.com>
2013-03-28 10:54:58 +0100

    Separated search page and search list, delegating to search item.

--- culturefeed_agenda/theme/culturefeed-search-page.tpl.php
+++ culturefeed_agenda/theme/culturefeed-search-page.tpl.php
@@ -3,24 +3,6 @@
  * @file 
  * Template for the main section of a search page. 
  * 
- * @var $search_result
- *   CultuurNet\Search\SearchResult
+ * @var $content
  */
-
-if (empty($variables['searchresult'])) {
-  print '<b>Geen resultaten gevonden</b>';
-}
-else {
-  
-  // Print the sortorder widget if you didn't use the block.
-  if (!empty($variables['sortorder_widget'])) {
-    print $variables['sortorder_widget'];
-  }
-  
-  foreach ($variables['searchresult']->getItems() as $item) {
-    $entity = $item->getEntity();
-    if ($entity instanceof CultureFeed_Cdb_Item_Event) {
-      print theme('culturefeed_event_summary', array('item' => $item));
-    }
-  }
-}
+print $content;
