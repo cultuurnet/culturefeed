9378a95d82459822a6cb97d9f134924d68cfc1d1
Jochen Stals <jochen.stals@wunderkraut.com>
2013-04-22 21:00:08 +0200

    added 2 template files.

---
+++ culturefeed_pages/theme/culturefeed-pages-block-admin-options.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file 
+ * Template file for the administrative options for a page.
+ * 
+ * @vars
+ * - $account The DrupalCultureFeed loggedin user
+ * - $page The Page object
+ * 
+ */
+?>
+
+<?php print $admin_menu; ?>
