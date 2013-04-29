aae947d9fa6852a27689e9babd77a2badcc6ae1a
Veerle Hanssens <veerlehanssens@Veerles-MacBook-Pro.local>
2013-04-29 11:54:57 +0200

    update culture feed submodule

--- culturefeed_social/theme/culturefeed-social-user-activity-list.tpl.php
+++ culturefeed_social/theme/culturefeed-social-user-activity-list.tpl.php
@@ -1,17 +1,15 @@
 <div class="activity-list">
 
-  <div class="items">
-    <ul>
-    <?php foreach ($items as $item): ?>
-      <li><?php print $item;?></li>
-    <?php endforeach; ?>
-    </ul>
-  </div>
+  <ul class="media-list">
+  <?php foreach ($items as $item): ?>
+    <li class="media"><?php print $item;?></li>
+  <?php endforeach; ?>
+  </ul>
 
   <?php if ($pager_path): ?>
   <div class="more-pager">
     <a class="pager-link" href="<?php print $pager_path; ?>" rel="no-follow">Lees oudere berichten</a>
   </div>
   <?php endif; ?>
-
+  
 </div>
