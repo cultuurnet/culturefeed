ccd8a044f087009ff4c31b7906a33eccd73501fa
zuuperman <nils.destoop@gmail.com>
2013-05-14 16:22:50 +0200

    Move drupal classes to file / class

---
+++ lib/Drupal/DrupalCultureFeed.php
@@ -0,0 +1,292 @@
+<?php
+
+/**
+ * @class
+ * Drupal layer for culturefeed.
+ */
+class DrupalCultureFeed extends DrupalCultureFeedBase {
+
+  protected static $consumer_instance;
+  protected static $user_instance;
+  protected static $logged_in_user;
+
+  public static function getInstance($token, $secret, $application_key = NULL, $shared_secret = NULL) {
+    $endpoint = variable_get('culturefeed_api_location', CULTUREFEED_API_LOCATION);
+    $oauth_client = self::getOAuthClient($endpoint, $token, $secret, $application_key, $shared_secret);
+
+    $cf = new CultureFeed($oauth_client);
+
+    if (self::isCacheEnabled()) {
+      $cf = new DrupalCultureFeed_Cache($cf, self::getLoggedInUserId());
+    }
+
+    return $cf;
+  }
+
+  public static function createUser(CultureFeed_User $user) {
+    self::getLoggedInUserInstance()->createUser($user);
+  }
+
+  public static function updateUser(CultureFeed_User $user, $fields = array()) {
+    self::getLoggedInUserInstance()->updateUser($user, $fields);
+  }
+
+  public static function deleteUser($id) {
+    self::getLoggedInUserInstance()->deleteUser($id);
+  }
+
+  public static function getUser($id, $private = FALSE) {
+    if ($private) {
+      return self::getLoggedInUserInstance()->getUser($id, TRUE, TRUE);
+    }
+    else {
+      return self::getConsumerInstance()->getUser($id, FALSE, FALSE);
+    }
+  }
+
+  public static function searchUsers(CultureFeed_SearchUsersQuery $query) {
+    return self::getConsumerInstance()->searchUsers($query);
+  }
+
+  public static function getSimilarUsers($id) {
+    return self::getConsumerInstance()->getSimilarUsers($id);
+  }
+
+  public static function uploadUserDepiction($id, $file_data) {
+    self::getLoggedInUserInstance()->uploadUserDepiction($id, $file_data);
+  }
+
+  public static function resendMboxConfirmationForUser($id) {
+    self::getLoggedInUserInstance()->resendMboxConfirmationForUser($id);
+  }
+
+  public static function updateUserPrivacy($id, CultureFeed_UserPrivacyConfig $privacy_config) {
+    self::getLoggedInUserInstance()->updateUserPrivacy($id, $privacy_config);
+  }
+
+  public static function getUserServiceConsumers($id) {
+    return self::getLoggedInUserInstance()->getUserServiceConsumers($id);
+  }
+
+  public static function revokeUserServiceConsumer($user_id, $consumer_id) {
+    self::getLoggedInUserInstance()->revokeUserServiceConsumer($user_id, $consumer_id);
+  }
+
+  public static function updateUserOnlineAccount($id, CultureFeed_OnlineAccount $account) {
+    self::getLoggedInUserInstance()->updateUserOnlineAccount($id, $account);
+  }
+
+  public static function getUserOnlineAccounts($application_key = NULL, $shared_secret = NULL) {
+    $cf_account = self::getLoggedInUser($application_key, $shared_secret);
+
+    $online_accounts = array();
+
+    if ($cf_account->holdsAccount) {
+      foreach ($cf_account->holdsAccount as $online_account) {
+        $online_accounts[$online_account->accountType] = $online_account;
+      }
+    }
+
+    return $online_accounts;
+  }
+
+  public static function getUserOnlineAccount($account_type) {
+    $online_accounts = self::getUserOnlineAccounts();
+
+    if (isset($online_accounts[$account_type])) {
+      return $online_accounts[$account_type];
+    }
+
+    return FALSE;
+  }
+
+  public static function deleteUserOnlineAccount($id, $account_type, $account_name) {
+    self::getLoggedInUserInstance()->deleteUserOnlineAccount($id, $account_type, $account_name);
+  }
+
+  public static function createActivity(CultureFeed_Activity $activity) {
+    return self::getLoggedInUserInstance()->createActivity($activity);
+  }
+
+  public static function postToSocial($id, $account_name, $account_type, $message, $image = NULL, $link = NULL) {
+    return self::getLoggedInUserInstance()->postToSocial($id, $account_name, $account_type, $message, $image, $link);
+  }
+
+  public static function updateActivity($id, $private) {
+    return self::getLoggedInUserInstance()->updateActivity($id, $private);
+  }
+
+  public static function deleteActivity($id) {
+    return self::getLoggedInUserInstance()->deleteActivity($id);
+  }
+
+  public static function deleteActivities($user_id, $node_id, $content_type, $activity_type) {
+    $query = new CultureFeed_SearchActivitiesQuery();
+    $query->type = $activity_type;
+    $query->contentType = $content_type;
+    $query->nodeId = $node_id;
+    $query->userId = $user_id;
+    $query->private = TRUE;
+
+    $activities = self::searchActivities($query);
+
+    if (empty($activities->objects)) {
+      return;
+    }
+
+    foreach ($activities->objects as $activity) {
+      self::deleteActivity($activity->id);
+    }
+  }
+
+  public static function searchActivityUsers($nodeId, $type, $contentType, $start = NULL, $max = NULL) {
+    return self::getConsumerInstance()->searchActivityUsers($nodeId, $type, $contentType, $start, $max);
+  }
+
+  public static function searchActivities(CultureFeed_SearchActivitiesQuery $query) {
+    if ($query->private) {
+      $data = self::getLoggedInUserInstance()->searchActivities($query);
+    }
+    else {
+      $data = self::getConsumerInstance()->searchActivities($query);
+    }
+
+    return $data;
+  }
+
+  public static function getTotalActivities($userId, $type_contentType, $private = FALSE) {
+    return self::getLoggedInUserInstance()->getTotalActivities($userId, $type_contentType, $private);
+  }
+
+  public static function getMailing($id) {
+    return self::getLoggedInUserInstance()->getMailing($id);
+  }
+
+  public static function createMailing(CultureFeed_Mailing $mailing) {
+    return self::getLoggedInUserInstance()->createMailing($mailing);
+  }
+
+  public static function updateMailing(CultureFeed_Mailing $mailing, $fields = array()) {
+    self::getLoggedInUserInstance()->updateMailing($mailing, $fields);
+  }
+
+  public static function disableMailing($id) {
+    self::getLoggedInUserInstance()->disableMailing($id);
+  }
+
+  public static function deleteMailing($id) {
+    self::getLoggedInUserInstance()->deleteMailing($id);
+  }
+
+  public static function getMailingList(CultureFeed_SearchMailingsQuery $query) {
+    return self::getLoggedInUserInstance()->getMailingList($query);
+  }
+
+  public static function sendTestMailing($user_id, $mailing_id) {
+    self::getLoggedInUserInstance()->sendTestMailing($user_id, $mailing_id);
+  }
+
+  public static function sendMailing($id) {
+    self::getLoggedInUserInstance()->sendMailing($id);
+  }
+
+  public static function searchMailings(CultureFeed_SearchMailingsQuery $query) {
+    return self::getLoggedInUserInstance()->searchMailings($query);
+  }
+
+  public static function subscribeToMailing($user_id, $mailing_id) {
+    self::getLoggedInUserInstance()->subscribeToMailing($user_id, $mailing_id);
+  }
+
+  public static function unsubscribeFromMailing($user_id, $mailing_id) {
+    self::getLoggedInUserInstance()->unsubscribeFromMailing($user_id, $mailing_id);
+  }
+
+  public static function getMailingSubscriptions($user_id) {
+    return self::getLoggedInUserInstance()->getMailingSubscriptions($user_id);
+  }
+
+  public static function getTopEvents($type, $max = 5) {
+    return self::getConsumerInstance()->getTopEvents($type, $max);
+  }
+
+  public static function getRecommendationsForUser($id, CultureFeed_RecommendationsQuery $query = NULL, $skip_cache = FALSE) {
+    return self::getLoggedInUserInstance()->getRecommendationsForUser($id, $query, $skip_cache);
+  }
+
+  public static function getRecommendationsForEvent($id, CultureFeed_RecommendationsQuery $query = NULL) {
+    return self::getConsumerInstance()->getRecommendationsForEvent($id, $query);
+  }
+
+  public static function evaluateRecommendation($id, $evaluation) {
+    self::getLoggedInUserInstance()->evaluateRecommendation($id, $evaluation);
+  }
+
+  /**
+   * @see ICultureFeed::getNotificationsCount()
+   */
+  public static function getNotificationsCount($userId, $dateFrom = NULL) {
+    return self::getLoggedInUserInstance()->getNotificationsCount($userId, $dateFrom);
+  }
+
+  /**
+   * @see ICultureFeed::getNotifications()
+   */
+  public static function getNotifications($userId, $params = array()) {
+    return self::getLoggedInUserInstance()->getNotifications($userId, $params);
+  }
+
+  public static function getNodeStatus($contentType, $nodeId, $userId) {
+    return self::getLoggedInUserInstance()->getNodeStatus($contentType, $nodeId, $userId);
+  }
+
+  public static function followNode($contentType, $nodeId, $userId) {
+    return self::getLoggedInUserInstance()->followNode($contentType, $nodeId, $userId);
+  }
+
+  public static function unFollowNode($contentType, $nodeId, $userId) {
+    return self::getLoggedInUserInstance()->unFollowNode($contentType, $nodeId, $userId);
+  }
+
+  public static function getUrlAddSocialNetwork($network, $destination = '') {
+    return self::getConsumerInstance()->getUrlAddSocialNetwork($network, $destination);
+  }
+
+  public static function getUrlChangePassword($id, $destination = '') {
+    return self::getConsumerInstance()->getUrlChangePassword($id, $destination);
+  }
+
+  public static function getUrlLogout($destination = '') {
+    return self::getConsumerInstance()->getUrlLogout($destination);
+  }
+
+  public static function userDidActivity($type, $nodeId, $contentType) {
+    $userDidActivity = &drupal_static(__FUNCTION__, array());
+
+    $user = self::getLoggedInUserId();
+
+    if (!isset($userDidActivity[$nodeId][$contentType][$user])) {
+      $query = new CultureFeed_SearchActivitiesQuery();
+      $query->nodeId = $nodeId;
+      $query->contentType = $contentType;
+      $query->userId = $user;
+      $query->private = TRUE;
+
+      $activities = self::searchActivities($query);
+
+      $userDidActivity[$nodeId][$contentType][$user] = $activities;
+    }
+
+    $activities = $userDidActivity[$nodeId][$contentType][$user];
+
+    if (!empty($activities->objects)) {
+      foreach ($activities->objects as $activity) {
+        if ($activity->type == $type) {
+          return TRUE;
+        }
+      }
+    }
+
+    return FALSE;
+  }
+}
---
+++ lib/Drupal/DrupalCultureFeedBase.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * @class
+ * Base class for getting the correct culturefeed layer.
+ */
+abstract class DrupalCultureFeedBase {
+
+  protected static $consumer_instance;
+
+  protected static $user_instance;
+
+  protected static $logged_in_account;
+
+  protected static $logged_in_user;
+
+
+  public static function isCacheEnabled() {
+
+    return variable_get('culturefeed_cache_status', CULTUREFEED_CACHE_DISABLED) == CULTUREFEED_CACHE_ENABLED;
+
+  }
+
+  public static function isCultureFeedUser($uid = NULL) {
+    if (!$uid) {
+      $account = self::getLoggedInAccount();
+    }
+    else {
+      $account = user_load($uid);
+    }
+
+    return isset($account->culturefeed_uid) && !empty($account->culturefeed_uid);
+  }
+
+  public static function getLoggedInUserId() {
+    if (self::getLoggedInAccount() && self::isCultureFeedUser()) {
+      return self::getLoggedInAccount()->culturefeed_uid;
+    }
+
+    return NULL;
+  }
+
+  public static function getLoggedInAccount() {
+    if (isset(self::$logged_in_account)) {
+      return self::$logged_in_account;
+    }
+
+    if (user_is_anonymous()) {
+      return NULL;
+    }
+
+    global $user;
+
+    self::$logged_in_account = user_load($user->uid);
+
+    return self::$logged_in_account;
+  }
+
+  public static function getLoggedInUser($application_key = NULL, $shared_secret = NULL, $reset = FALSE) {
+    if (!$application_key) {
+      $application_key = variable_get('culturefeed_api_application_key', '');
+      $shared_secret = variable_get('culturefeed_api_shared_secret', '');
+    }
+
+    if (!$reset && isset(static::$logged_in_user[$application_key])) {
+      return static::$logged_in_user[$application_key];
+    }
+
+    if (!self::isCultureFeedUser()) {
+      return NULL;
+    }
+
+    static::$logged_in_user[$application_key] = static::getLoggedInUserInstance($application_key, $shared_secret)->getUser(self::getLoggedInUserId(), TRUE, TRUE);
+
+    return static::$logged_in_user[$application_key];
+  }
+
+  /**
+   * @param string|null $application_key
+   * @param string|null $shared_secret
+   * @return ICultureFeed
+   * @throws Exception
+   */
+  public static function getLoggedInUserInstance($application_key = NULL, $shared_secret = NULL) {
+    if (!$application_key) {
+      $application_key = variable_get('culturefeed_api_application_key', '');
+      $shared_secret = variable_get('culturefeed_api_shared_secret', '');
+    }
+
+    if (isset(self::$user_instance[$application_key])) {
+      return self::$user_instance[$application_key];
+    }
+
+    $account = self::getLoggedInAccount();
+
+    if (!isset($account->tokens) ||
+    !isset($account->tokens[$application_key]) ||
+    !isset($account->tokens[$application_key]->token) ||
+    !isset($account->tokens[$application_key]->secret)) {
+      throw new Exception('Not a valid token set.');
+    }
+
+    $token = $account->tokens[$application_key];
+
+    static::$user_instance[$application_key] = static::getInstance($token->token, $token->secret, $application_key, $shared_secret);
+
+    return static::$user_instance[$application_key];
+  }
+
+  public static function getConsumerInstance($application_key = NULL, $shared_secret = NULL) {
+    if (!$application_key) {
+      $application_key = variable_get('culturefeed_api_application_key', '');
+      $shared_secret = variable_get('culturefeed_api_shared_secret', '');
+    }
+
+    if (isset(static::$consumer_instance[$application_key])) {
+      return static::$consumer_instance[$application_key];
+    }
+
+    static::$consumer_instance[$application_key] = static::getInstance(NULL, NULL, $application_key, $shared_secret);
+
+    return static::$consumer_instance[$application_key];
+  }
+
+  /**
+   * Get an OAuthClient instance for CultureFeed.
+   * @param string $endpoint
+   *   Endpoint for the client.
+   */
+  public static function getOAuthClient($endpoint, $token, $secret, $application_key = NULL, $shared_secret = NULL) {
+    if (!$application_key) {
+      $application_key = variable_get('culturefeed_api_application_key', '');
+      $shared_secret = variable_get('culturefeed_api_shared_secret', '');
+    }
+
+    $oauth_client = new CultureFeed_DefaultOAuthClient($application_key, $shared_secret, $token, $secret);
+
+    $oauth_client->setEndpoint($endpoint);
+
+    $uri = @parse_url($endpoint);
+    $proxy_server = variable_get('proxy_server', '');
+    if ($proxy_server && _drupal_http_use_proxy($uri['host'])) {
+      $http_client = new CultureFeed_DefaultHttpClient();
+
+      $http_client->setProxyServer($proxy_server);
+      $http_client->setProxyPort(variable_get('proxy_port', 8080));
+      $http_client->setProxyUsername(variable_get('proxy_username', ''));
+      $http_client->setProxyPassword(variable_get('proxy_password', ''));
+
+      $oauth_client->setHttpClient($http_client);
+    }
+
+    return $oauth_client;
+  }
+
+}
