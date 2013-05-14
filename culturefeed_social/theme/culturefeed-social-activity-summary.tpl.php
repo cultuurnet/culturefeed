9ab098a421e8e6b88a1776d9f40d3c9f2dcd4718
Jochen Stals <jochen.stals@wunderkraut.com>
2013-05-14 15:02:35 +0200

    finished activity counts.

--- culturefeed_social/theme/culturefeed-social-activity-summary.tpl.php
+++ culturefeed_social/theme/culturefeed-social-activity-summary.tpl.php
@@ -7,8 +7,33 @@
  *   - $event_14 (Comments for events), $event_15, $book_14, ... etc.
  *   
  * To make it easier, some variables are already prepared to use in this file. 
- * Those variables are listed beneath.
+ * Those variables are listed beneath. Ofcourse we can always count the counts together 
+ * for all other custom calculations.
  *   
  * @var
- * - 
+ * Variables for content types in general (all content types).
+ * - total_14
+ * - total_15
+ * 
+ * Variables specific for an activity type
+ * - books_total_<activity_type_number>
+ * - pages_total_<activity_type_number>
+ * - activities_total_<activity_type_number>
+ * 
+ * E.g.:
+ * - total_14
+ * - activities_total_14
+ * - books_total_14
+ * - pages_total_14
  */
+?>
+
+<h2>Mijn activiteiten</h2>
+
+<p>
+  <strong><?php print $activities_total_15 ?></strong> activiteiten aangeraden<br />
+  <strong><?php print $books_total_15 ?></strong> boeken aangeraden<br />
+  <strong><?php print $pages_total_18 ?></strong> pagina's die ik volg<br />
+  <strong><?php print $total_14 ?></strong> beoordelingen geschreven<br />
+  aangeraden
+</p>
