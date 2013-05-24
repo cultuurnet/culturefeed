f8a84b694dbac34c49b8df0df495c347ae656458
Luk Dens <luk.dens@cultuurnet.be>
2013-05-24 17:09:25 +0200

    theme my pages memberships

--- culturefeed_pages/theme/culturefeed-pages-page-my-page.tpl.php
+++ culturefeed_pages/theme/culturefeed-pages-page-my-page.tpl.php
@@ -2,14 +2,18 @@
   <a href="<?php print $user_url; ?>">Bekijk profiel</a>
 </div>
 
-<?php if (!empty($my_pages)): ?>
 <h2>Pagina's waar ik lid van ben</h2>
-<?php print $my_pages; ?>
+<?php if (!empty($my_pages)): ?>
+  <?php print $my_pages; ?>
+<?php else: ?>
+  <div class="alert alert-info"><strong>Je bent nog geen lid van een pagina.</strong><br />Zoek (of maak) de pagina van je werkgever en word lid. als je een nieuwe pagina maakt, ben je automatische en lid en beheerder van de pagina.</div>
 <?php endif; ?>
 
-<?php if (!empty($i_follow)): ?>
 <h2>Pagina's die ik volg</h2>
-<?php print $i_follow; ?>
+<?php if (!empty($i_follow)): ?>
+  <?php print $i_follow; ?>
+<?php else: ?>
+  <div class="alert alert-info"><strong>Je volgt nog geen pagina's.</strong><br />Zoek en volg pagina's die je interesseren en ontvang een melding als er relevante updates zijn.</div>
 <?php endif; ?>
 
 <h2>Zoek nieuwe pagina's:</h2>
