615777d0a04e95c62494088de119717246e74bc6
zuuperman <nils.destoop@gmail.com>
2013-05-28 15:04:24 +0200

    Added methods to interface

--- culturefeed_search_ui/lib/Drupal/CultureFeedSearchPageInterface.php
+++ culturefeed_search_ui/lib/Drupal/CultureFeedSearchPageInterface.php
@@ -1,6 +1,9 @@
 <?php
 
 interface CultureFeedSearchPageInterface {
+  public function setResultsPerPage($resultsPerPage);
+  public function setFullPage($fullPage);
+  public function setPagerType($pagerType);
   public function loadPage();
   public function getDrupalTitle();
 }
