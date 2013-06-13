e255ea10807fe82fa8000bb5a8182c5dff3c9bb6
zuuperman <nils.destoop@gmail.com>
2013-06-13 14:25:13 +0200

    Promotion list

--- culturefeed_userpoints_ui/theme/culturefeed-userpoints-ui-promotions-list-item.tpl.php
+++ culturefeed_userpoints_ui/theme/culturefeed-userpoints-ui-promotions-list-item.tpl.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @file 
+ * @file
  * Template for one promotion list item.
- * 
- * @vars 
+ *
+ * @vars
  *   Content
  *     $title
  *     $id
@@ -18,10 +18,10 @@
  *     $remove_link
  *     $select_list (the list / dropdown for the count)
  *     $select_btn
- *     $volume_constraint 
+ *     $volume_constraint
  *       Whether the volume is exceeded during the call.
- *   
- * @note 
+ *
+ * @note
  *   The culturefeed-userpoints-item-<?php print $id ?> should be available as wrapper
  *   for ajax requests updating the userpoint promotions.
  */
@@ -29,35 +29,33 @@
 ?>
 
 <div class="<?php print $classes ?>" id="culturefeed-userpoints-item-wrapper-<?php print $id ?>">
-  
+
   <?php print $real_points ?><br />
   <?php print $title ?> (<?php print $link ?>)<br />
   Geldig van <?php print $cashingPeriodBegin; ?> <?php print (!empty($cashingPeriodEnd) ? ' tot ' . $cashingPeriodEnd : '') ?>
 
   <div id="culturefeed-userpoints-item-<?php print $id ?>">
-  
+
     <?php if ($volume_constraint): ?>
-    
+
     Dit voordeel is niet meer in voorraad.
     <?php print $remove_link ?>
-    
+
     <?php elseif ($can_exchange): ?>
-    
+
     <?php print $select_list; ?>
     <?php print $select_btn; ?>
     <?php print $remove_link ?>
-    
+
     <?php else: ?>
-    
-    Je hebt onvoldoende punten voor dit geschenk. 
-    
+
+    Je hebt onvoldoende punten voor dit geschenk.
+
     <a href="/">Hoe extra punten sparen?</a>
-    
+
     <?php endif; ?>
-    
-    <?php drupal_render_children($form); ?>
-    
+
   </div>
-  
+
 </div>
 
