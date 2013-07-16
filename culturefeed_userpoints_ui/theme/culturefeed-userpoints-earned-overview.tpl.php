02e6db9ec5a9db1979c52427f6044efb67aa6376
Veerle Hanssens <veerle.hanssens@cultuurnet.be>
2013-07-16 10:29:06 +0200

    theme - puntenoverzicht

--- culturefeed_userpoints_ui/theme/culturefeed-userpoints-earned-overview.tpl.php
+++ culturefeed_userpoints_ui/theme/culturefeed-userpoints-earned-overview.tpl.php
@@ -1,10 +1,27 @@
+<p><strong><span class="lead"><?php print $total_points; ?> punten</span></strong> <?php print $exchange_link; ?></p>
 
-<p>Intro tekst komt hier</p>
+<p class="muted"><small>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. <a href="#">Meer informatie</a></small></p>
 
-Totaal aantal punten: <?php print $total_points; ?> <?php print $exchange_link; ?>
-<br/>
-<?php foreach ($items as $item): ?>
-  <?php print $item['date']; ?> -
-  <?php print $item['points']; ?> -
-  <?php print $item['description']; ?><br/>
-<?php endforeach; ?>
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($items as $item): ?>
+      <tr>
+        <td class="muted"><small><?php print $item['date']; ?></small></td>
+        <td><small><strong><?php print $item['points']; ?></strong></small></td>
+        <td><?php print $item['description']; ?></td>
+      </tr>
+    <?php endforeach; ?>  
+  </tbody>
+</table>
+
+
+
+
+
