32c9f845ff77d6d242f685d1dd027fe1efab7d5a
Luk Dens <luk.dens@cultuurnet.be>
2013-07-19 14:10:10 +0200

    use tpl's from custom theme as default

--- uitpas_ui/theme/uitpas-ui-activity.tpl.php
+++ uitpas_ui/theme/uitpas-ui-activity.tpl.php
@@ -1,23 +1,159 @@
-<div class="uitpas_ui_activity_details">
-  <?php if ($icons): ?>
-    <div class="uitpas_ui_activity_details_icons"><?php print $icons; ?></div>
-  <?php endif; ?>
-  <div class="uitpas_ui_activity_details_general">
-    <div class="uitpas_ui_activity_details_general_headings"><?php print $headings; ?></div>
-    <div class="uitpas_ui_activity_details_general_shortdescription"><?php print $shortdescription; ?></div>
-    <div class="uitpas_ui_activity_details_general_when"><?php print $when; ?></div>
-    <div class="uitpas_ui_activity_details_general_where"><?php print $where; ?></div>
-    <div class="uitpas_ui_activity_details_general_price"><?php print $price; ?></div> 
-    <!-- TEST -->   
-    <?php if ($organisation): ?>
-      <div class="uitpas_ui_activity_details_general_organisation"><?php print $organisation; ?></div>
+<div class="cnapi-detail">
+
+  <div class="detail-top">
+      
+    <?php if ($headings) : ?> 
+      <span class="event-categories">
+        <span class="icon"></span> 
+        <?php print $headings; ?> 
+      </span>
     <?php endif; ?>
-    <div class="uitpas_ui_activity_details_general_links"><?php print $links; ?></div>
+    
   </div>
-  <?php if ($image): ?>
-    <div class="uitpas_ui_activity_details_image"><?php print $image; ?></div>
-  <?php endif; ?>
-  <?php if ($icons): ?>
-    <div class="uitpas_ui_activity_details_legend"><?php print $icons; ?></div>
-  <?php endif; ?>
+  
+  <div class="detail-middle">
+    <div class="detail-left">
+      
+      <?php if ($shortdescription) : ?>
+        <div class="description"><?php print $shortdescription ?></div>
+      <?php endif; ?>
+      
+      <dl>
+      
+      <?php if ($when) : ?>
+        <dt>Wanneer</dt>
+        <dd>
+          <?php print $when ?>
+        </dd>
+      <?php endif; ?>
+ 
+      <?php if ($where) : ?>
+        <dt>Waar</dt>
+        <dd>
+          <?php print $where; ?> 
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($performers) : ?>
+        <dt>Wie</dt>
+        <dd>
+          <?php print implode(', ', $performers); ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($price) : ?>
+        <dt>Prijs</dt>
+        <dd>
+          <?php print $price ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($reservation) : ?>
+        <dt>Reserveren</dt>
+        <dd>
+        <?php if (isset($reservation['mail'])) : ?>
+          <?php print $reservation['mail'] ?><br />
+        <?php endif; ?>
+        <?php if (isset($reservation['phone'])) : ?>
+          <?php if (isset($reservation['fax'])) : ?>Telefoon: <?php endif; ?><?php print $reservation['phone'] ?><br />
+        <?php endif; ?>
+        <?php if (isset($reservation['fax'])) : ?>
+          Fax: <?php print $reservation['fax'] ?>
+        <?php endif; ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($tickets) : ?>
+        <dt></dt>
+        <dd>
+          <?php print implode(', ', $tickets); ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($organiser && !$location_is_organiser) : ?>
+        <dt>Organisatie</dt>
+        <dd>
+          <?php print $organisation ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($contact) : ?>
+        <dt>Contact</dt>
+        <dd>
+        <?php if (isset($contact['mail'])) : ?>
+          <?php print $contact['mail'] ?><br />
+        <?php endif; ?>
+        <?php if (isset($contact['phone'])) : ?>
+          <?php if (isset($contact['fax'])) : ?>Telefoon: <?php endif; ?><?php print $contact['phone'] ?><br />
+        <?php endif; ?>
+        <?php if (isset($contact['fax'])) : ?>
+          Fax: <?php print $contact['fax'] ?>
+        <?php endif; ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($links) : ?>
+        <dt>Links</dt>
+        <dd>
+          <?php print $links; ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($agefrom) : ?>
+        <dt>Leeftijd</dt>
+        <dd>
+          <?php print $agefrom; ?>
+          <?php if ($for_children) : ?>
+            <?php print $vlieg_image ?>
+          <?php endif; ?>
+        </dd>
+      <?php endif; ?>
+        
+      <?php if ($targetaudiences) : ?>
+        <dt>Doelgroep</dt>
+        <dd>
+          <?php print implode(', ', $targetaudiences); ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($facilities) : ?>
+        <dt>Voorzieningen</dt>
+        <dd>
+          <?php print implode(', ', $facilities); ?>            
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($longdescription) : ?>
+        <dt>Meer info</dt>
+        <dd>
+          <?php print $longdescription ?>          
+        </dd>
+      <?php endif; ?>
+      
+      </dl>
+    
+    </div>
+  
+    <div class="detail-right">
+      <?php if ($images) : ?>
+        <div class="image-holder">
+          <?php foreach ($images as $image) : ?>
+            <div class="image">
+              <?php print $image['image'] ?>
+            </div>
+            <?php if (!empty($image['copyright'])) : ?><div class="copyright">&copy; <?php print $image['copyright'] ?></div><?php endif; ?>
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?> 
+      <?php if ($icons): ?>
+        <div class="uitpas-advantages">
+          <?php print $icons; ?>
+        </div>       
+      <?php endif; ?>            
+    </div>
+  </div>
+  
+  <div class="detail-bottom">
+  </div>
+  
 </div>
--- uitpas_ui/theme/uitpas-ui-actor.tpl.php
+++ uitpas_ui/theme/uitpas-ui-actor.tpl.php
@@ -1,17 +1,99 @@
-<div class="uitpas_ui_actor_details">
-  <?php if ($icons): ?>
-    <div class="uitpas_ui_actor_details_icons"><?php print $icons; ?></div>
-  <?php endif; ?>
-  <div class="uitpas_ui_actor_details_general">
-    <div class="uitpas_ui_actor_details_general_shortdescription"><?php print $shortdescription; ?></div>
-    <div class="uitpas_ui_actor_details_general_address"><?php print $address; ?></div>
-    <div class="uitpas_ui_actor_details_general_contact"><?php print $contact; ?></div>
-    <div class="uitpas_ui_actor_details_general_links"><?php print $links; ?></div>
+<div class="cnapi-detail">
+  
+  <div class="detail-middle">
+    <div class="detail-left">
+      
+      <?php if ($shortdescription) : ?>
+        <div class="description"><?php print $shortdescription ?></div>
+      <?php endif; ?>
+      
+      <dl>
+      
+      <?php if ($address) : ?>
+        <dt>Waar</dt>
+        <dd>
+          <?php print $address ?> 
+          <?php if ($coords) : ?>
+          <div id="show-map"></div>
+          <div id="map" class="hidden">
+            <a href="http://maps.google.com/maps?hl=nl&amp;q=<?php print $coords['lat'] ?>, <?php print $coords['lng'] ?>"><img src="http://maps.google.com/maps/api/staticmap?center=<?php print $coords['lat'] ?>, <?php print $coords['lng'] ?>&amp;zoom=15&amp;markers=<?php print $coords['lat'] ?>, <?php print $coords['lng'] ?>&amp;size=264x200&amp;sensor=false"></a>
+          </div>
+          <?php endif; ?> 
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($reservation) : ?>
+        <dt>Reserveren</dt>
+        <dd>
+        <?php if (isset($reservation['mail'])) : ?>
+          <?php print $reservation['mail'] ?><br />
+        <?php endif; ?>
+        <?php if (isset($reservation['phone'])) : ?>
+          <?php if (isset($reservation['fax'])) : ?>Telefoon: <?php endif; ?><?php print $reservation['phone'] ?><br />
+        <?php endif; ?>
+        <?php if (isset($reservation['fax'])) : ?>
+          Fax: <?php print $reservation['fax'] ?>
+        <?php endif; ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($contact) : ?>
+        <dt>Contact</dt>
+        <dd>
+          <?php print $contact; ?>
+        </dd>
+      <?php endif; ?>
+      
+      <?php if ($links) : ?>
+        <dt>Links</dt>
+        <dd>
+          <?php print $links; ?>
+        </dd>
+      <?php endif; ?>
+      
+      </dl>
+      
+      <?php if ($facilities) : ?>
+        <dt>Voorzieningen</dt>
+        <dd>
+          <?php print implode(', ', $facilities); ?>
+        </dd>
+      <?php endif; ?>
+
+        
+      <?php if ($longdescription) : ?>
+        <dt>Meer info</dt>
+        <dd>
+          <div class="description full">
+            <?php print $longdescription ?>
+          </div>
+        </dd>
+      <?php endif; ?> 
+             
+      <?php if ($legend): ?>
+        <div class="uitpas-advantages clearfix">
+          <?php print $legend; ?>
+        </div>
+      <?php endif; ?>   
+    
+    </div>
+  
+    <div class="detail-right">
+      <?php if ($images) : ?>
+        <div class="image-holder">
+          <?php foreach ($images as $image) : ?>
+            <div class="image">
+              <?php print $image['image'] ?>
+            </div>
+            <?php if (!empty($image['copyright'])) : ?><div class="copyright">&copy; <?php print $image['copyright'] ?></div><?php endif; ?>
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?>       
+    </div>
   </div>
-  <?php if ($image): ?>
-    <div class="uitpas_ui_actor_details_image"><?php print $image; ?></div>
-  <?php endif; ?>
-  <?php if ($legend): ?>
-    <div class="uitpas_ui_actor_details_legend"><?php print $legend; ?></div>
-  <?php endif; ?>
+  
+  <div class="detail-bottom">
+  </div>
+
 </div>
+
