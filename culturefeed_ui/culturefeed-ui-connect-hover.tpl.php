822e8b46ee6e3325986370647cc29910e62f6ab9
svenhoutmeyers <sven@cultuurnet.be>
2013-07-22 18:14:45 +0200

    translate culturefeed_ui to english

--- culturefeed_ui/culturefeed-ui-connect-hover.tpl.php
+++ culturefeed_ui/culturefeed-ui-connect-hover.tpl.php
@@ -1,5 +1,5 @@
 <div class="tooltip-login hover">
-  <p>Om verder te gaan moet je aangemeld zijn met je UiTiD.</p>
+  <p><?php print t('To continue you have to be logged on with your UiTiD.') ?></p>
   <span class="facebook"><?php print $link_facebook ?></span>
-  <span class="other">Of via <?php print $link_twitter ?>, <?php print $link_google ?> of <?php print $link_email ?></span>
+  <span class="other"><?php print t('Or via') ?> <?php print $link_twitter ?>, <?php print $link_google ?> <?php print t('or') ?>  <?php print $link_email ?></span>
 </div>
--- culturefeed_ui/culturefeed-ui-connect.tpl.php
+++ culturefeed_ui/culturefeed-ui-connect.tpl.php
@@ -1,13 +1,13 @@
 <h3>
-  <span>Kiezen wordt makkelijker</span>
-  <span>dankzij UiTiD</span>
+  <span><?php print t('Choosing your events will be simplified') ?></span>
+  <span><?php print t('dankzij UiTiD') ?></span>
 </h3>
-<p>Maak een UiTiD aan en:</p>
+<p><?php print t('Create an UiTiD account and:') ?></p>
 <ul>
-  <li>ontvang <strong>persoonlijke tips</strong> op jouw maat</li>
-  <li>ontdek wat <strong>vrienden en gelijkaardige gebruikers</strong> leuk vinden</li>
-  <li>blijf <strong>als eerste op de hoogte</strong> van je favoriete activiteiten</li>
+  <li><?php print t('receive <strong>personal tips</strong> only for you') ?></li>
+  <li><?php print t('discover what <string>your friends and similar users</strong> like') ?></li>
+  <li><?php print t('be <strong>the first to know</strong> updates about your favorite activities' ?></li>
 </ul>
 
 <span class="facebook"><?php print $link_facebook ?></span>
-<span class="other">OF via <?php print $link_twitter ?>, <?php print $link_google?> of <?php print $link_email?></span>
+<span class="other"><?php print t('Or via') ?> <?php print $link_twitter ?>, <?php print $link_google?> <?php print t('or') ?> <?php print $link_email?></span>
