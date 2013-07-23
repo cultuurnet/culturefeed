97fe96539c28d9ff27e79fbd038ab16f44fbb39e
zuuperman <nils.destoop@gmail.com>
2013-07-23 12:41:31 +0200

    Only do geolocate on click

--- culturefeed_agenda/js/search_block.js
+++ culturefeed_agenda/js/search_block.js
@@ -4,7 +4,21 @@
   Drupal.CultureFeed.Agenda = {};
   
   $(document).ready(function() {
-    Drupal.CultureFeed.geolocate('#current-location', 'input[id="geolocate-coordinates"]', 'input[id="geolocate-city"]');
+    
+    var $nearby_checkbox = $('#culturefeed-agenda-search-block-form').find('input[name="nearby"]');
+    $nearby_checkbox.bind('change', function() {
+      
+      if (this.checked) {
+        $('#culturefeed-agenda-search-block-form').find('#current-location').before(document.createTextNode(": "));
+        Drupal.CultureFeed.geolocate('#current-location', 'input[id="geolocate-coordinates"]', 'input[id="geolocate-city"]');        
+      }
+      
+    });
+    
+    if ($nearby_checkbox.is(':checked')) {
+      $nearby_checkbox.trigger('change');
+    }
+    
   });
   
 })(jQuery);
