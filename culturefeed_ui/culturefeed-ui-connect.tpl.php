c0178f447c4dcf739f734a744c05c57c852f80cf
zuuperman <nils.destoop@gmail.com>
2013-07-24 12:02:25 +0200

    Added cnapi heading import

--- culturefeed_ui/culturefeed-ui-connect.tpl.php
+++ culturefeed_ui/culturefeed-ui-connect.tpl.php
@@ -6,7 +6,7 @@
 <ul>
   <li><?php print t('receive <strong>personal tips</strong> only for you') ?></li>
   <li><?php print t('discover what <string>your friends and similar users</strong> like') ?></li>
-  <li><?php print t('be <strong>the first to know</strong> updates about your favorite activities' ?></li>
+  <li><?php print t('be <strong>the first to know</strong> updates about your favorite activities') ?></li>
 </ul>
 
 <span class="facebook"><?php print $link_facebook ?></span>
