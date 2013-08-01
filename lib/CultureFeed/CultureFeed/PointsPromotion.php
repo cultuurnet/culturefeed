9b3c379a6fa3f7731a94f8837be87c0fefaf961f
zuuperman <nils.destoop@gmail.com>
2013-08-01 11:05:08 +0200

    Catch CASHIN_PROMOTION_NOT_ALLOWED with a custom error message

--- lib/CultureFeed/CultureFeed/PointsPromotion.php
+++ lib/CultureFeed/CultureFeed/PointsPromotion.php
@@ -6,75 +6,80 @@
 class CultureFeed_PointsPromotion {
 
   /**
+   * The cashing in of a promotion failed.
+   */
+  const CASHIN_PROMOTION_NOT_ALLOWED = 'CASHIN_PROMOTION_NOT_ALLOWED';
+
+  /**
    * cashInState: Not possible points constraint.
    */
   const NOT_POSSIBLE_POINTS_CONSTRAINT = 'NOT_POSSIBLE_POINTS_CONSTRAINT';
-  
+
   /**
    * cashInState: Not possible user volume constraint.
    */
   const NOT_POSSIBLE_USER_VOLUME_CONSTRAINT = 'NOT_POSSIBLE_USER_VOLUME_CONSTRAINT';
-  
+
   /**
    * cashInstate: Possible to exchange points.
    */
   const POSSIBLE = 'POSSIBLE';
-  
+
   /**
    * Periodtype: ABSOLUTE
    */
   const ABSOLUTE = "ABSOLUTE";
-  
+
   /**
    * @var String constant.
    */
   public $cashInState;
-  
+
   /**
    * @var Boolean
    */
   public $cashedIn;
-  
+
   /**
    * @var string.
    */
   public $id;
-  
+
   /**
    * @var date.
    */
   public $cashingPeriodBegin;
-  
+
   /**
    * @var date
    */
   public $cashingPeriodEnd;
-  
+
   /**
    * @var date
    */
   public $creationDate;
-  
+
   /**
    * @var integer
    */
   public $maxAvailableUnits;
-  
+
   /**
    * @var CultureFeed_PeriodConstraint
    */
   public $periodConstraint = NULL;
-  
+
   /**
    * @var double.
    */
   public $points;
-  
+
   /**
    * @var Boolean.
    */
   public $inSpotlight;
-  
+
   /**
    * @var string.
    */
@@ -89,28 +94,28 @@ class CultureFeed_PointsPromotion {
    * @var string.
    */
   public $description2;
-  
+
   /**
    * @var array of picture urls.
    */
   public $pictures = array();
-  
+
   /**
    * @var integer.
    */
   public $unitsTaken;
-  
+
   /**
    * Extract an array useable as data in POST requests that expect PointsPromotion info.
    *
    * @return array
-   *   Associative array representing the PointsPromotion object. 
+   *   Associative array representing the PointsPromotion object.
    *   For documentation of the structure, check the Culture Feed API documentation.
    */
   public function toPostData() {
     // For most properties we can rely on get_object_vars.
     $data = get_object_vars($this);
-    
+
     // Represent cashingPeriodBegin as a W3C date.
     if (isset($data['cashingPeriodBegin'])) {
       $data['cashingPeriodBegin'] = date('c', $data['cashingPeriodBegin']);
@@ -120,13 +125,13 @@ class CultureFeed_PointsPromotion {
     if (isset($data['creationDate'])) {
       $data['creationDate'] = date('c', $data['creationDate']);
     }
-    
+
     // Booleans to string.
     $data['cashedIn'] = $data['cashedIn'] ? "true" : "false";
     $data['inSpotlight'] = $data['inSpotlight'] ? "true" : "false";
-  
+
     $data = array_filter($data);
-  
+
     return $data;
   }
 
@@ -141,11 +146,11 @@ class CultureFeed_PointsPromotion {
     if (empty($element->id)) {
       throw new CultureFeed_ParseException('id missing for PointsPromotions element');
     }
-    
+
     if (empty($element->title)) {
       throw new CultureFeed_ParseException('title missing for PointsPromotions element');
     }
-    
+
     if (empty($element->points)) {
       throw new CultureFeed_ParseException('points missing for PointsPromotions element');
     }
@@ -169,11 +174,11 @@ class CultureFeed_PointsPromotion {
 
     // Set relations.
     if (!empty($element->pictures) && !empty($element->pictures->picture)) {
-    
+
       foreach ($element->pictures->picture as $picture) {
         $pointsPromotion->pictures[] = (string) $picture;
       }
-    
+
     }
 
     $periodType = $element->xpath_str('periodConstraint/periodType');
@@ -183,6 +188,6 @@ class CultureFeed_PointsPromotion {
     }
 
     return $pointsPromotion;
-    
+
   }
 }
