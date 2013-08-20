4faf3d770f597fba05c9ecb59a747350f00c3758
Jochen Stals <jochen.stals@wunderkraut.com>
2013-08-20 12:22:11 +0200

    changed description so it works with subject. I am checking if this is correct.

--- culturefeed_mailing/theme/culturefeed-mailing-user-profile-admin-item.tpl.php
+++ culturefeed_mailing/theme/culturefeed-mailing-user-profile-admin-item.tpl.php
@@ -1,4 +1,4 @@
 <p class="lead"><strong><?php print $title; ?></strong></p>
-<p class="muted"><small><strong><?php print $frequency; ?></strong></small> <?php print $description; ?> - Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+<p class="muted"><small><strong><?php print $frequency; ?></strong></small> <?php print $description; ?></p>
 <p><?php print $optin_toggle; ?></p>
 <hr />
