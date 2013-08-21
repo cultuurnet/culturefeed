45f3be4495fc862fccee91b5543ff520d556e8c3
Veerle Hanssens <veerle.hanssens@cultuurnet.be>
2013-08-21 09:47:11 +0200

    theme - facet & suggestions

--- culturefeed_search_ui/theme/culturefeed-search-suggestions.tpl.php
+++ culturefeed_search_ui/theme/culturefeed-search-suggestions.tpl.php
@@ -4,9 +4,9 @@
  *   - $suggestions (key = search string, value = search url)
  */
 ?>
-<di>
-<h2 class="block-title"><?php print t('Did you mean'); ?>: </h2>
-<?php foreach ($suggestions as $suggestion_words => $suggestion_url): ?>
-  <a href="<?php print $suggestion_url; ?>"><?php print $suggestion_words ?></a>
-<?php endforeach;?>
-</div>
+<p>
+  <strong><?php print t('Did you mean'); ?></strong>
+  <?php foreach ($suggestions as $suggestion_words => $suggestion_url): ?>
+    <a href="<?php print $suggestion_url; ?>"><?php print $suggestion_words ?></a>
+    <?php endforeach;?>
+</p>
