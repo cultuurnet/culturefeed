965aba5248be0677efe5a50576ef08f46d094f2b
Veerle Hanssens <veerle.hanssens@cultuurnet.be>
2013-08-27 13:20:48 +0200

    theme - facets - remove icons

--- culturefeed_search_ui/theme/culturefeed-search-active-filters-item.tpl.php
+++ culturefeed_search_ui/theme/culturefeed-search-active-filters-item.tpl.php
@@ -6,8 +6,6 @@
 ?>
 
 <span class="filter">
-    <span class="filter-label"><?php print check_plain($label); ?></span>
-    <a href="<?php print $url; ?>" title="Verwijder filter '<?php print check_plain($label); ?>'">
-      <i class="icon-remove icon-large text-red"></i><span class="hidden">verwijder</span>
-    </a>
+  <span class="filter-label"><?php print check_plain($label); ?></span> 
+  <a href="<?php print $url; ?>" class="filter-remove"><strong>&times;</strong></a>
 </span>
