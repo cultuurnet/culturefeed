4ff0e3d2822dc4ad17f1a33900f4cc2fffd43be0
Jochen Stals <jochen.stals@wunderkraut.com>
2013-08-28 12:38:22 +0200

    aanpassing van interface.

--- culturefeed_search_ui/lib/Drupal/CultureFeedSearchPageInterface.php
+++ culturefeed_search_ui/lib/Drupal/CultureFeedSearchPageInterface.php
@@ -1,6 +1,10 @@
 <?php
 
 interface CultureFeedSearchPageInterface {
+
+  public function initialize();
+  public function getFacetComponent();
+  public function getSearchResult();
   public function setResultsPerPage($resultsPerPage);
   public function setFullPage($fullPage);
   public function setPagerType($pagerType);
