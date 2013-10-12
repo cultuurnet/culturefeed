17e80669bba5cdf5c98970c2adf68740faddeca2
Kristof Coomans <kristof@coomans.eu>
2013-10-12 10:04:02 +0200

    UITPAS-663: parse id and nick elements correctly

--- lib/CultureFeed/CultureFeed/Uitpas/Counter/Member.php
+++ lib/CultureFeed/CultureFeed/Uitpas/Counter/Member.php
@@ -18,8 +18,12 @@ class CultureFeed_Uitpas_Counter_Member extends CultureFeed_Uitpas_ValueObject {
 
   public static function createFromXML(CultureFeed_SimpleXMLElement $object) {
     $member = new CultureFeed_Uitpas_Counter_Member();
-    $member->id = $object->xpath_str('ns2:id');
-    $member->nick = $object->xpath_str('ns3:nick');
+
+    $object->registerXPathNamespace('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns');
+    $object->registerXPathNamespace('foaf', 'http://xmlns.com/foaf/0.1/');
+
+    $member->id = $object->xpath_str('rdf:id');
+    $member->nick = $object->xpath_str('foaf:nick');
 
     return $member;
   }
