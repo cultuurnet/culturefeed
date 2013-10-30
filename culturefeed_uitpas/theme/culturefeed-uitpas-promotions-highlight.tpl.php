71f0e4ca8107947ea04aec3cb00ffed566104e88
Hans Langouche <hans@2dotstwice.be>
2013-10-30 14:44:05 +0100

    DECUPAS-13 public blocks

---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-promotions-highlight.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display culturefeed uitpas promotions 
+ * highlight.
+ *
+ * Available variables:
+ * - $promotions: The list of promotions.
+ * - $more: Linked url of the full list.
+ */
+?>
+<div class="promotions_highlight">
+  <?php print $promotions; ?>
+  <div class="more"><?php print $more; ?>
+</div>
---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-recent-actions.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display culturefeed uitpas recent actions.
+ *
+ * Available variables:
+ * - $actions: The list of actions.
+ */
+?>
+<div class="recent-actions">
+  <div class="list"><?php print $actions; ?></div>
+</div>
