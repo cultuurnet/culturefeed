883073b0c26d325e250991a09cdd8d0bd660233a
svenhoutmeyers <sven@cultuurnet.be>
2013-10-30 20:43:06 +0100

    adding facet-city-only block

--- culturefeed_search_ui/lib/Drupal/CultureFeedSearchPage.php
+++ culturefeed_search_ui/lib/Drupal/CultureFeedSearchPage.php
@@ -427,9 +427,16 @@ class CultureFeedSearchPage {
       // Check if postal was present.
       $city_parts = explode(' ', $params['location']);
       if (is_numeric($city_parts[0])) {
-        $distance = isset($params['distance']) ? $params['distance'] : '0.00001';
-
-        $this->parameters[] = new Parameter\Spatial\Zipcode($city_parts[0], $distance);
+        $distance = isset($params['distance']) ? $params['distance'] : FALSE;
+        
+        // If category_actortype_id we assume that we search on pages (on day we have to fix)
+        if (isset($params['facet']['category_actortype_id'])) {
+          $this->parameters[] = new Parameter\FilterQuery('zipcode' . ':' . $city_parts[0]);
+        }
+        else {
+          $this->parameters[] = new Parameter\Spatial\Zipcode($city_parts[0], $distance);
+        } 
+        
       }
       else {
         $location = '"' . str_replace('"', '\"', $params['location']) . '"';
