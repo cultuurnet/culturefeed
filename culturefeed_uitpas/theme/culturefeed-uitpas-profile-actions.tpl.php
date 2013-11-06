45c03b7e718d1424c4ae75b52c84f6f86b6975cb
Kristof Coomans <kristof@coomans.eu>
2013-11-06 20:58:53 +0100

    DECUPAS-32 Adapt CultureFeed_Uitpas_Passholder::currentCard to API changes.

---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-profile-actions.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display culturefeed uitpas profile actions.
+ *
+ * Available variables:
+ * - $actions: The list of actions.
+ */
+?>
+<div class="profile_actions">
+  <?php print $actions; ?>
+</div>
