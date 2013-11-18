22940d6e14ea8066f1110c87067ccd711193811f
Hans Langouche <hans@2dotstwice.be>
2013-11-18 13:00:59 +0100

    DECUPAS-16 feedback

--- culturefeed_uitpas/theme/culturefeed-uitpas-promotion.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-promotion.tpl.php
@@ -7,16 +7,28 @@
  *
  * Available variables:
  * - $points: The number of points.
+ * - $image
  * - $period: The period.
  * - $location: The location.
  * - $available: The availability.
+ * - $description1.
+ * - $description2.
  */
 ?>
 <div class="promotion_details">
+  <?php if ($image): ?>
+  <div class="image"><?php print $image; ?></div>
+  <?php endif; ?>
   <div class="points"><?php print $points; ?></div>
   <?php if ($period): ?>
   <div class="period"><?php print $period; ?></div>
   <?php endif; ?>
   <div class="location"><?php print $location; ?></div>
   <div class="available"><?php print $available; ?></div>
+  <?php if ($description1): ?>
+  <div class="description1"><?php print $description1; ?></div>
+  <?php endif; ?>
+  <?php if ($description2): ?>
+  <div class="description2"><?php print $description2; ?></div>
+  <?php endif; ?>
 </div>
