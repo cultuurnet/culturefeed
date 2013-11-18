ad963d448f612d0d659b5406154be14211af0417
Hans Langouche <hans@2dotstwice.be>
2013-11-18 13:17:08 +0100

    DECUPAS-21 feedback

--- culturefeed_uitpas/theme/culturefeed-uitpas-advantage.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-advantage.tpl.php
@@ -5,15 +5,27 @@
  * Default theme implementation to display culturefeed uitpas advantage details.
  *
  * Available variables:
+ * - $image.
  * - $period: The period.
  * - $location: The location.
  * - $available: The availability.
+ * - $description1.
+ * - $description2.
  */
 ?>
 <div class="advantage_details">
+  <?php if ($image): ?>
+  <div class="image"><?php print $image; ?></div>
+  <?php endif; ?>
   <?php if ($period): ?>
   <div class="period"><?php print $period; ?></div>
   <?php endif; ?>
   <div class="location"><?php print $location; ?></div>
   <div class="available"><?php print $available; ?></div>
+  <?php if ($description1): ?>
+  <div class="description1"><?php print $description1; ?></div>
+  <?php endif; ?>
+  <?php if ($description2): ?>
+  <div class="description2"><?php print $description2; ?></div>
+  <?php endif; ?>
 </div>
