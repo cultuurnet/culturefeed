dc8d5da64bc22d800d0663c8e3e0e51319896213
Hans Langouche <hans@2dotstwice.be>
2013-12-10 16:23:59 +0100

    DECUPAS-48

---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-register-where.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display culturefeed uitpas register where.
+ *
+ * Available variables:
+ * - $pos: Points of sale.
+ * - $info: Info.
+ */
+?>
+<div class="register_where">
+  <div class="register_where_pos"><?php print $pos; ?></div>
+  <div class=register_where_info"><?php print $info; ?></div>
+</div>
