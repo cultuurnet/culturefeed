715c7df8b0b812a125afd1f5a5032edee15fe8df
Hans Langouche <hans@2dotstwice.be>
2013-12-14 22:28:21 +0100

    DECUPAS-129

--- culturefeed_uitpas/theme/culturefeed-uitpas-profile-details.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-profile-details.tpl.php
@@ -5,7 +5,8 @@
  * Default theme implementation to display culturefeed uitpas profile details.
  *
  * Available variables:
- * - $uitpas_number.
+ * - $uitpas_title.
+ * - $uitpas_numbers.
  * - $intro.
  * - $intro.
  * - $first_name.
@@ -28,40 +29,46 @@
  * - $outro.
  */
 ?>
-  
-<p class="uitpas_number"><?php print $uitpas_number; ?></p>
 
-<?php if ($intro): ?>
-<p class="intro"><?php if ($intro): ?></p>
-<?php endif; ?>
+<div class="profile_uitpas">
 
-<ul class="data">
-  <li><?php print $first_name; ?></li>
-  <li><?php print $last_name; ?></li>
-  <li><?php print $dob; ?></li>
-  <li><?php print $pob; ?></li>
-  <li><?php print $gender; ?></li>
-  <li><?php print $nationality; ?></li>
-  <li><?php print $street; ?></li>
-  <li><?php print $zip; ?></li>
-  <li><?php print $city; ?></li>
-  <li><?php print $tel; ?></li>
-  <li><?php print $mobile; ?></li>
-  <li><?php print $email; ?></li>
-</ul>
+  <h2><?php print $uitpas_title; ?></h2>
 
-<!--
-<?php if ($kansen_statuut && $kansen_statuut_valid_end_date): ?>
-<div class="status">
-  <h3><?php print $status_title; ?></h3>
-  <p><?php print $status_valid_till; ?></p>
-  <?php if ($memberships): ?>
-  <p><?php print $memberships; ?></p>
+  <p class="uitpas_numbers"><?php print $uitpas_numbers; ?></p>
+
+  <?php if ($intro): ?>
+  <p class="intro"><?php print $intro; ?></p>
   <?php endif; ?>
-</div>
-<?php endif; ?>
--->
 
-<?php if ($outro): ?>
-<p><?php if ($outro): ?></p>
-<?php endif; ?>
+  <ul class="data">
+    <li><?php print $first_name; ?></li>
+    <li><?php print $last_name; ?></li>
+    <li><?php print $dob; ?></li>
+    <li><?php print $pob; ?></li>
+    <li><?php print $gender; ?></li>
+    <li><?php print $nationality; ?></li>
+    <li><?php print $street; ?></li>
+    <li><?php print $zip; ?></li>
+    <li><?php print $city; ?></li>
+    <li><?php print $tel; ?></li>
+    <li><?php print $mobile; ?></li>
+    <li><?php print $email; ?></li>
+  </ul>
+
+  <!--
+  <?php if ($kansen_statuut && $kansen_statuut_valid_end_date): ?>
+  <div class="status">
+    <h3><?php print $status_title; ?></h3>
+    <p><?php print $status_valid_till; ?></p>
+    <?php if ($memberships): ?>
+    <p><?php print $memberships; ?></p>
+    <?php endif; ?>
+  </div>
+  <?php endif; ?>
+  -->
+
+  <?php if ($outro): ?>
+  <p><?php print $outro; ?></p>
+  <?php endif; ?>
+
+</div>
