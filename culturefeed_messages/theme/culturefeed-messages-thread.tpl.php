d785527edf3f5769418a1c48f4aa3a4ab8ae7aac
zuuperman <nils.destoop@gmail.com>
2014-03-18 16:32:11 +0100

    Added default message: user of page

--- culturefeed_messages/theme/culturefeed-messages-thread.tpl.php
+++ culturefeed_messages/theme/culturefeed-messages-thread.tpl.php
@@ -1,7 +1,7 @@
 <p><?php print format_plural($message_count, '@count message', '@count messages') ?> | </span><?php print $delete_link; ?></p>
-<p><strong><?php print t('From'); ?>:</strong> <?php print $sender_link; ?> <?php print $sender_page; ?></p>
-<p><strong><?php print t('To'); ?>:</strong> <?php print $recipient_links; ?> <?php print $recipient_page; ?></p>
-<p><strong><?php print t('Subject'); ?>:</strong> <?php $subject ? print $subject : print t('No subject') ; ?></p>
+<p><strong><?php print t('From'); ?>:</strong> <?php print $sender; ?></p>
+<p><strong><?php print t('To'); ?>:</strong> <?php print $recipient ?></p>
+<p><strong><?php print t('Subject'); ?>:</strong> <?php $subject ? print $subject  : print t('No subject') ; ?></p>
 <hr />
 <?php foreach ($messages as $message): ?>
   <?php print $message; ?>
