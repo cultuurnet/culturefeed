2a41e085ba5227293cc6a18fea1a4829a735a11b
Veerle Hanssens <veerle.hanssens@cultuurnet.be>
2014-03-19 16:33:44 +0100

    CF-188: update layout pages-events-admin-page

--- culturefeed_pages/theme/culturefeed-pages-events-admin-page.tpl.php
+++ culturefeed_pages/theme/culturefeed-pages-events-admin-page.tpl.php
@@ -1,22 +1,18 @@
+<h3><?php print t('Activities'); ?></h3>
 <p><?php print $view_page_link; ?></p>
 
-<div class="info">
+<?php if (!empty($items)): ?>
+
   <p><?php print t('The <strong>list</strong> below shows all the <strong>activities</strong> that are <strong>currently published</strong> on your page.') ?>
   <br />
   <?php print t('Past or unpublished activities can be consulted via <a href="http://www.uitdatabank.be" target="_blank"> UiTdatabank </a>.'); ?></p>
-</div>
-
-<hr />
-
-<h3><?php print t('Activities'); ?></h3>
-<p><a href="http://www.uitdatabank.be" target="_blank"><?php print t('Add a new activity via UiTdatabank'); ?></a> (<?php print t('You can sign in with your current username and password.'); ?>)</p>
-  
-<?php if (!empty($items)): ?>
 
   <?php print $items; ?>
 
   <?php print theme('pager') ?>
 
 <?php else: ?>
-    <?php print t('There are currently no published activities available for your page. Past or unpublished activities can be consulted via <a href="http://www.uitdatabank.be" target="_blank"> UiTdatabank </a>.') ?>
+    <p><?php print t('There are currently no published activities available for your page. Past or unpublished activities can be consulted via <a href="http://www.uitdatabank.be" target="_blank"> UiTdatabank </a>.') ?></p>
 <?php endif; ?>
+
+<p><a href="http://www.uitdatabank.be" target="_blank"><?php print t('Add a new activity via UiTdatabank'); ?></a> (<?php print t('You can sign in with your current username and password.'); ?>)</p>
