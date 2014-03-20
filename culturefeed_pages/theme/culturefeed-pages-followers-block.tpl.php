f5e98759125b590fb5e01a03c1cd6ec3a036154b
Veerle Hanssens <veerle.hanssens@cultuurnet.be>
2014-03-20 15:39:31 +0100

    CF-1CF-188: update layout & labels

--- culturefeed_pages/theme/culturefeed-pages-followers-block.tpl.php
+++ culturefeed_pages/theme/culturefeed-pages-followers-block.tpl.php
@@ -1,33 +1,24 @@
-<div class="followers-block">
-  <h3><?php print $title; ?></h3>
+<h5><?php print $title; ?></h5>
 
-  <?php if (!empty($followers)): ?>
+<?php if (!empty($followers)): ?>
+  <p><?php print $num_followers ?> <?php print t('followers'); ?> - <?php print $follow_link ?></p>
 
-    <div class="followed-times"><?php print $num_followers ?> x</div>
+  <ul>
+  <?php foreach ($followers as $follower): ?>
+    <li>
+      <a href="<?php print $follower['url'] ?>"><?php print $follower['name']; ?></a>
+    </li>
+  <?php endforeach; ?>
+  </ul>
 
-    <ul>
-    <?php foreach ($followers as $follower): ?>
-      <li>
-        <a href="<?php print $follower['url'] ?>"><?php print $follower['name']; ?></a>
-        <?php if ($follower['picture']): ?>
-        <?php print theme('image', array('path' => $follower['picture'] . '?maxwidth=100')) ?>
-        <?php endif; ?>
-      </li>
-    <?php endforeach; ?>
-    </ul>
+<?php else: ?>
+  <p><?php print t('This page has no followers yet.'); ?></p>
+<?php endif; ?>
 
-  <?php else: ?>
-    <p><?php print t('This page has no followers yet.'); ?></p>
-  <?php endif; ?>
 
-  <?php
-  /**
-   * .follow-PAGEID is used to refresh that part of the html. You can use it
-   * freely as you want. E.g. wrap the text above in it or not.
-   */
-   ?>
-  <div class="follow-<?php print $page->getId() ?>">
-    <?php print $follow_link ?>
-  </div>
-
-</div>
+<?php
+/**
+ * .follow-PAGEID is used to refresh that part of the html. You can use it
+ * freely as you want. E.g. wrap the text above in it or not.
+ */
+ ?>
