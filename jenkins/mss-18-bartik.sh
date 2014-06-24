8f37514407740d8bdcae7c80ba4374985183e0ed
Sander Vleugels <sandervleugels@gmail.com>
2014-06-24 13:33:34 +0200

    MSS-18 jenkins scripts toevoegen

---
+++ jenkins/mss-18-bartik.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+drush dis jquery_update -y;
+drush vset theme_default bartik;
---
+++ jenkins/mss-18-bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+drush en jquery_update -y;
+drush vset theme_default culturefeed_bootstrap;
