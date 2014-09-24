38df699af63d802a9b9631c4e8abea8847f3f59b
Stijn Swaanen <swaanenstijn@gmail.com>
2014-09-24 14:33:02 +0200

    UPS-989 php mermoy limit

---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-advantages.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display culturefeed uitpas advantages
+ * promotions.
+ *
+ * Available variables:
+ * - $advantages_table: The list of advantages.
+ * - $info: Info text.
+ */
+?>
+<div class="advantages">
+  <div class="advantages_table"><?php print $advantages_table; ?></div>
+  <div class="info"><?php print $info; ?></div>
+</div>
