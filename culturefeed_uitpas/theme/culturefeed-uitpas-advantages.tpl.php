d0d9c1dc93e03f8dcc34dd6698afbd8696ff89dc
Stijn Swaanen <swaanenstijn@gmail.com>
2014-10-14 10:03:39 +0200

    UPS-1432 add menu to advantages page

--- culturefeed_uitpas/theme/culturefeed-uitpas-advantages.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-advantages.tpl.php
@@ -10,6 +10,14 @@
  * - $info: Info text.
  */
 ?>
+
+<div id="advantages_link">
+  <ul>
+    <li><a href="/promotions"><?php print t('Promotions') ?></li>
+    <li><a href="/advantages"><?php print t('Welcome Advantages') ?></li>
+  </ul>
+</div>
+
 <div class="advantages">
   <div class="advantages_table"><?php print $advantages_table; ?></div>
   <div class="info"><?php print $info; ?></div>
