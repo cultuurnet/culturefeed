9c49066448f50ff44bf1664141cb3c374832cd8f
Stijn Swaanen <swaanenstijn@gmail.com>
2014-10-15 14:22:09 +0200

    UPS-982 optimisation my promotions and my advantages

---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-profile-promotions.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display culturefeed uitpas advantages
+ * promotions.
+ *
+ * Available variables:
+ * - $promotions_table: The list of promotions.
+ * - $info: Info text.
+ */
+?>
+<div class="profile-promotions">
+  <div class="promotions_table"><?php print $profile_promotions_table; ?></div>
+
+  <div class="info"><?php print $info; ?></div>
+</div>
