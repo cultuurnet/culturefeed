36a9bc834775ce62fafeee32b9fb707474ad17df
Kristof Coomans <kristof@coomans.eu>
2014-10-23 17:15:21 +0200

    Replace culturefeed classes with the separate library

--- lib/CultureFeed/CultureFeed/HttpException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class CultureFeed_HttpException extends Exception {
-  public $body;
-
-  function __construct($body, $code) {
-    parent::__construct('The reponse for the HTTP request was not 200. ' . $body, $code);
-  }
-}
