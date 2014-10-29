4da4d082926982655cf2eb52dcc35f2afe96dccb
Bram Cordie <bramcordie@gmail.com>
2014-10-29 09:30:52 +0100

    Add json-LD as a content-type (MIME/Internet Media Type)

---
+++ culturefeed_udb3/src/JsonLdSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\culturefeed_udb3\JsonLdSubscriber.
+ */
+
+namespace Drupal\culturefeed_udb3;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Subscribes to the kernel request event to add HAL media types.
+ */
+class JsonLdSubscriber implements EventSubscriberInterface {
+
+    /**
+     * Registers Json-LD formats with the Request class.
+     *
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+     *   The event to process.
+     */
+    public function onKernelRequest(GetResponseEvent $event) {
+        $request = $event->getRequest();
+        $request->setFormat('ld_json', 'application/ld+json');
+    }
+
+    /**
+     * Registers the methods in this class that should be listeners.
+     *
+     * @return array
+     *   An array of event listener definitions.
+     */
+    static function getSubscribedEvents() {
+        $events[KernelEvents::REQUEST][] = array('onKernelRequest', 40);
+        return $events;
+    }
+
+}
