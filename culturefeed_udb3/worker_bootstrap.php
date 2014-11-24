bf72356cc639a095e703bb6e89f8a56d6045e192
Kristof Coomans <kristof@coomans.eu>
2014-11-24 16:26:27 +0100

    Call bootstrap code after fork

--- culturefeed_udb3/worker_bootstrap.php
+++ culturefeed_udb3/worker_bootstrap.php
@@ -16,19 +16,24 @@ while ($depth < $drupal_root_depth) {
   $depth++;
 }
 
-$autoloader = require_once 'core/vendor/autoload.php';
-require_once 'sites/all/vendor/autoload.php';
+// Bootstrap drupal after the parent forks its process and is ready to perform.
+Resque_Event::listen('afterFork', function() {
 
-try {
-  $request = Request::createFromGlobals();
-  $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
-  $kernel->prepareLegacyRequest($request);
+    $autoloader = require_once 'core/vendor/autoload.php';
+    require_once 'sites/all/vendor/autoload.php';
 
-  $command_bus = $kernel->getContainer()->get('culturefeed_udb3.event_command_bus');
-  QueueJob::setCommandBus($command_bus);
-}
-catch (Exception $e) {
-  $message = 'Error';
-  print $message;
-  throw $e;
-}
+    try {
+      $request = Request::createFromGlobals();
+      $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
+      $kernel->prepareLegacyRequest($request);
+
+      $command_bus = $kernel->getContainer()->get(
+        'culturefeed_udb3.event_command_bus'
+      );
+      QueueJob::setCommandBus($command_bus);
+    } catch (Exception $e) {
+      $message = 'Error';
+      print $message;
+      throw $e;
+    }
+  });
