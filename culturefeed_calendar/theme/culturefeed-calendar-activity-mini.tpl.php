516a2226efee7bdb49e43e7a4487d9d559400836
Dries Declercq <dries.declercq@gmail.com>
2014-12-09 17:09:29 +0100

    UiT-16: Plan and delete activities.

--- culturefeed_calendar/theme/culturefeed-calendar-activity-mini.tpl.php
+++ culturefeed_calendar/theme/culturefeed-calendar-activity-mini.tpl.php
@@ -9,6 +9,10 @@
     <strong class="hidden-xs hidden-sm"><?php print $title; ?></strong>
     <span><?php print $when; ?></span>
   </td>
-  <td class="col-lg-3 col-md-3"><a href="<?php print $plan_link['url'] ?>"><?php print $plan_link['text']; ?></a></td>
-  <td class="col-lg-2 col-md-2"><?php print $remove_button; ?></td>
+  <td class="col-lg-3 col-md-3">
+    <a href="<?php print $edit_link['url'] ?>"><?php print $edit_link['text']; ?></a>
+  </td>
+  <td class="col-lg-2 col-md-2">
+    <a href="<?php print $delete_link['url'] ?>"><?php print $delete_link['text']; ?></a>
+  </td>
 </tr>
