eab6882600579c611cd18a67c213e45f143e1c21
zuuperman <nils.destoop@gmail.com>
2014-12-15 16:54:21 +0100

    Also take over hidePopup

--- culturefeed_agenda/js/actor_autocomplete.js
+++ culturefeed_agenda/js/actor_autocomplete.js
@@ -26,8 +26,35 @@
     if ($(this.input).hasClass('auto-submit-field')) {
       window.location.href = $(node).data('autocompleteValue');
     }
+    else {
+      this.input.value = $(node).data('autocompleteValue');
+    }
   };
 
+  /**
+   * Hides the autocomplete suggestions.
+   */
+  Drupal.jsAC.prototype.hidePopup = function (keycode) {
+    // Select item if the right key or mousebutton was pressed.
+    if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
+
+      if ($(this.input).hasClass('auto-submit-field')) {
+        window.location.href = $(node).data('autocompleteValue');
+      }
+      else {
+        this.input.value = $(this.selected).data('autocompleteValue');
+      }
+
+    }
+    // Hide popup.
+    var popup = this.popup;
+    if (popup) {
+      this.popup = null;
+      $(popup).fadeOut('fast', function () { $(popup).remove(); });
+    }
+    this.selected = false;
+    $(this.ariaLive).empty();
+  };
 
   /**
    * Attach behaviors for the actor autocomplete.
