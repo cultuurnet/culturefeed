6a0aa91d423256ff3323854b96e28ac7670790de
zuuperman <nils.destoop@gmail.com>
2014-12-15 16:14:49 +0100

    Move saving of cookie to general space + do ajax call to get closest x

--- culturefeed_agenda/js/actor_autocomplete.js
+++ culturefeed_agenda/js/actor_autocomplete.js
@@ -10,7 +10,10 @@
     $.ajax({
       url : Drupal.settings.culturefeed.actorsSuggestUrl,
       success : function(data, status) {
-        $('#actor-search-suggest').html(data);
+        console.log(data);
+        if (data.success) {
+          $('#actor-search-suggest').html(data.data);
+        }
       }
     })
 
@@ -29,39 +32,13 @@
   /**
    * Attach behaviors for the actor autocomplete.
    */
-  Drupal.behaviors.culturefeedActorAutocomplete = {
-    attach: function (context, setings) {
-      if ($.cookie('Drupal.visitor.uitid.userLocation') !== null) {
-        Drupal.Culturefeed.getNearbyActors();
-      };
-    }
-  }
-
-  // Temp code, untill homepage story is started.
   $(document).ready(function() {
-    if (navigator.geolocation) {
-
-      //if the browser supports geolocations get along and execute
-      navigator.geolocation.getCurrentPosition( function (position) {
-        //build up the querie for the google api
-        var geocoder = new google.maps.Geocoder();
-        var lat = parseFloat(position.coords.latitude);
-        var lng = parseFloat(position.coords.longitude);
-        var latlng = new google.maps.LatLng(lat, lng);
-
-        // execute the request and send the respons to addpostalcode function
-        geocoder.geocode({'latLng': latlng}, function (response) {
-
-          var location = {
-            latitude : response[0].geometry.location.lat(),
-            longitude : response[0].geometry.location.lng(),
-          }
-
-          $.cookie('Drupal.visitor.uitid.userLocation', JSON.stringify(location));
-        });
-      });
-
+    if ($.cookie('Drupal.visitor.uitid.userLocation') !== null) {
+      Drupal.Culturefeed.getNearbyActors();
     }
-  })
+    else {
+      Drupal.CultureFeed.geolocate(Drupal.Culturefeed.getNearbyActors);
+    }
+  });
 
 })(jQuery);
--- js/common.js
+++ js/common.js
@@ -17,7 +17,37 @@
         var latlng = new google.maps.LatLng(lat, lng);
 
         // execute the request and send the respons to addpostalcode function
-        geocoder.geocode({'latLng': latlng}, response_callback);
+        geocoder.geocode({'latLng': latlng}, function(response) {
+
+          // Set the info also in a cookie.
+          var place = response[0];
+          for (i = 0; i < place.address_components.length; i++) {
+
+            if (place.address_components[i].types[0] == 'postal_code') {
+              current_postal = place.address_components[i].long_name;
+            }
+            else if(place.address_components[i].types[0] == 'locality') {
+              current_city = place.address_components[i].long_name;
+            }
+
+          }
+
+          var location = {
+            latitude : place.geometry.location.lat(),
+            longitude : place.geometry.location.lng(),
+            city : current_city,
+            postal : current_postal
+          }
+          $.cookie('Drupal.visitor.uitid.userLocation', JSON.stringify(location), { path : '/' });
+
+          // Call the response callback function.
+          if (response_callback) {
+            response_callback(response);
+          }
+
+        });
+
+
       });
 
     }
