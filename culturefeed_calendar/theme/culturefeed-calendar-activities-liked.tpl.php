3eac97af098ca86fe77da047cb1b6f89600c2e00
Luk Dens <luk.dens@cultuurnet.be>
2015-01-14 14:14:39 +0100

    copy: fix dutch string

--- culturefeed_calendar/theme/culturefeed-calendar-activities-liked.tpl.php
+++ culturefeed_calendar/theme/culturefeed-calendar-activities-liked.tpl.php
@@ -6,7 +6,7 @@
 ?>
 
 <div class="calendar-activities-liked-wrapper">
-  <h4><?php print t('Nog niet ingepland'); ?></h4>
+  <h4><?php print t('Unscheduled events'); ?></h4>
 
   <?php foreach ($activities as $activity): ?>
     <?php print theme('culturefeed_calendar_activity_mini', array('activity' => $activity)); ?>
