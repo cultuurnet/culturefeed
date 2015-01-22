2e6866ed28cda91127e1c4da6390b61bd6aaa523
zuuperman <nils.destoop@gmail.com>
2015-01-22 11:33:55 +0100

    UIT-14: Code review

--- culturefeed_calendar/theme/culturefeed-calendar-settings-page.tpl.php
+++ culturefeed_calendar/theme/culturefeed-calendar-settings-page.tpl.php
@@ -7,7 +7,7 @@
 
 <?php print l(t('< Back to my OuTcalendar'), $back_to_calendar_url); ?>
 <h3><?php print t('Settings for your OuTcalendar'); ?></h3>
-<?php print render($calendar_settings_form); ?>
+<?php print render($settings_form); ?>
 
 
 
