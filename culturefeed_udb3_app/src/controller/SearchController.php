6143341be133767c5e1cd83daa45439a78ca9417
Bram Cordie <bramcordie@gmail.com>
2015-01-27 10:49:48 +0100

    Add udb3-angular modules and render a basic search page

---
+++ culturefeed_udb3_app/src/controller/SearchController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\culturefeed_udb3_app\Controller\SearchController.
+ */
+
+namespace Drupal\culturefeed_udb3_app\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class SearchController extends ControllerBase implements ContainerInjectionInterface {
+
+  public function search() {
+
+    $baseTag = [
+      '#type' => 'html_tag',
+      '#tag' => 'base',
+      '#attributes' => ['href' => '/']
+    ];
+
+    $renderArray = [
+      '#theme' => 'udb_search',
+      '#content' => Array('Hello', 'world'),
+      '#attached' => [
+        'library' => [
+          'culturefeed_udb3_app/udb3-drupal-app'
+        ]
+      ]
+    ];
+    
+    return $renderArray;
+
+  }
+
+}
