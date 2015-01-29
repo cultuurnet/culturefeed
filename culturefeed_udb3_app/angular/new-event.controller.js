b57a39d3c45e18623cdaa127656410bc763dafd5
Bram Cordie <bramcordie@gmail.com>
2015-01-29 10:29:12 +0100

    Explicitly inject new event dependencies and remove udb3-angular.js/.css copies

--- culturefeed_udb3_app/angular/new-event.controller.js
+++ culturefeed_udb3_app/angular/new-event.controller.js
@@ -11,44 +11,49 @@
   angular
     .module('udbApp')
     .constant('moment', moment)
-    .controller('NewEventCtrl', function (udbApi, $scope, $q, $http, appConfig, moment) {
-      udbApi.newEvent = function (name, location, date) {
-        var deferred = $q.defer();
-        $http.post(
-          appConfig.baseUrl + 'events',
-          {
-            name: name,
-            location: location,
-            date: moment(date).format('YYYY-MM-DDTHH:mm:ssZ')
-          },
-          {
-            withCredentials: true,
-            headers: {
-              'Content-Type': 'application/json'
-            }
+    .controller('NewEventCtrl', NewEventCtrl);
+
+  NewEventCtrl.$inject = ['udbApi', '$scope', '$q', '$http', 'appConfig', 'moment'];
+
+  function NewEventCtrl(udbApi, $scope, $q, $http, appConfig, moment) {
+    udbApi.newEvent = function (name, location, date) {
+      var deferred = $q.defer();
+      $http.post(
+        appConfig.baseUrl + 'events',
+        {
+          name: name,
+          location: location,
+          date: moment(date).format('YYYY-MM-DDTHH:mm:ssZ')
+        },
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json'
           }
-        ).success(function (data) {
-            deferred.resolve(data.eventId);
-          })
-          .error(function (data) {
-            deferred.reject(data.error);
-          });
+        }
+      ).success(function (data) {
+          deferred.resolve(data.eventId);
+        })
+        .error(function (data) {
+          deferred.reject(data.error);
+        });
 
-        return deferred.promise;
-      };
+      return deferred.promise;
+    };
 
-      $scope.newEvent = function (name, location, date) {
-        console.log(name);
-        console.log(location);
-        console.log(date);
+    $scope.newEvent = function (name, location, date) {
+      console.log(name);
+      console.log(location);
+      console.log(date);
 
-        udbApi.newEvent(name, location, date)
-          .then(function (eventId) {
-            window.alert(eventId);
-          },
-          function (error) {
-            window.alert(error);
-          });
-      };
-    });
+      udbApi.newEvent(name, location, date)
+        .then(function (eventId) {
+          window.alert(eventId);
+        },
+        function (error) {
+          window.alert(error);
+        });
+    };
+  }
+
 })();
