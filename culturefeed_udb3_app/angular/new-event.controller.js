10c5fa798e1d1e3cb8971088ca458f3cce49dbef
Kristof Coomans <kristof@coomans.eu>
2015-01-29 07:55:17 +0100

    III-137: Post values from event form to backend

---
+++ culturefeed_udb3_app/angular/new-event.controller.js
@@ -0,0 +1,54 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name udbApp.controller:NewEventCtrl
+   * @description
+   * # NewEventCtrl
+   * udbApp controller
+   */
+  angular
+    .module('udbApp')
+    .constant('moment', moment)
+    .controller('NewEventCtrl', function (udbApi, $scope, $q, $http, appConfig, moment) {
+      udbApi.newEvent = function (name, location, date) {
+        var deferred = $q.defer();
+        $http.post(
+          appConfig.baseUrl + 'events',
+          {
+            name: name,
+            location: location,
+            date: moment(date).format('YYYY-MM-DDTHH:mm:ssZ')
+          },
+          {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        ).success(function (data) {
+            deferred.resolve(data.eventId);
+          })
+          .error(function (data) {
+            deferred.reject(data.error);
+          });
+
+        return deferred.promise;
+      };
+
+      $scope.newEvent = function (name, location, date) {
+        console.log(name);
+        console.log(location);
+        console.log(date);
+
+        udbApi.newEvent(name, location, date)
+          .then(function (eventId) {
+            window.alert(eventId);
+          },
+          function (error) {
+            window.alert(error);
+          });
+      };
+    });
+})();
