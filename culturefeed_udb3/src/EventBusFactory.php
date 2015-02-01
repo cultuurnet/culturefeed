890ee6bf6d4e9b8d7655bb9836f1f6ed598d6676
Kristof Coomans <kristof@coomans.eu>
2015-02-01 09:23:46 +0100

    Put use statement on top, and remove unused property

--- culturefeed_udb3/src/EventBusFactory.php
+++ culturefeed_udb3/src/EventBusFactory.php
@@ -7,15 +7,14 @@ namespace Drupal\culturefeed_udb3;
 
 
 use CultuurNet\UDB3\SimpleEventBus;
+use Broadway\EventHandling\EventBusInterface;
 
 class EventBusFactory{
-
-  private $subscribers;
 
   public function get($subscribers) {
     $bus = new SimpleEventBus();
 
-    $bus->beforeFirstPublication(function (\Broadway\EventHandling\EventBusInterface $eventBus) use ($subscribers) {
+    $bus->beforeFirstPublication(function (EventBusInterface $eventBus) use ($subscribers) {
       foreach ($subscribers as $subscriber) {
         $eventBus->subscribe(\Drupal::service($subscriber));
       }
