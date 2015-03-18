2142ba44e560d8d85eabe0fadb0e3f2af390a6e9
Luk Dens <luk.dens@cultuurnet.be>
2015-03-18 18:02:49 +0100

    typo translate string culturefeed_calendar

--- culturefeed_calendar/theme/culturefeed-calendar-button-hover.tpl.php
+++ culturefeed_calendar/theme/culturefeed-calendar-button-hover.tpl.php
@@ -1,5 +1,5 @@
 <div class="tooltip-login hover">
-  <p><?php print t('You have temporarily added events to your calendar. Log in and save the events to you personal calendar.') ?></p>
+  <p><?php print t('You have temporarily added events to your calendar. Log in and save the events to your personal calendar.') ?></p>
   <hr>
   <p>
     <span><strong><?php print l(t('Log in'), $url, $options) ?></strong></span>
