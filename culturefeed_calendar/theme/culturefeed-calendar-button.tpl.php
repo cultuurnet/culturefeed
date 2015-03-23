e4e84135637ebfc12f0c04b815144f8bf8d13f68
Jochen Stals <stalski@322618.no-reply.drupal.org>
2015-03-24 00:09:37 +0100

    uit-36: Als bezoeker zie ik een melding ipv kalenderbuttons indien een event is afgelopen

--- culturefeed_calendar/theme/culturefeed-calendar-button.tpl.php
+++ culturefeed_calendar/theme/culturefeed-calendar-button.tpl.php
@@ -7,8 +7,12 @@
 ?>
 
 <div class="<?php print $classes; ?>" data-eventid="<?php print $event_id; ?>">
-  <?php if (isset($button['description'])) : ?>
-    <?php print $button['description']; ?>
+  <?php if ($finished) : ?>
+    <?php print $finished_message; ?>
+  <?php else : ?>
+    <?php if (isset($button['description'])) : ?>
+      <?php print $button['description']; ?>
+    <?php endif; ?>
+    <?php print l($button['text'], $button['path'], $button['options']); ?>
   <?php endif; ?>
-  <?php print l($button['text'], $button['path'], $button['options']); ?>
 </div>
