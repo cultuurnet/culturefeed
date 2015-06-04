dcddc99c748839f8015b6a5f672da55e71fa9412
HnLn <hans.langouche@gmail.com>
2015-06-04 09:57:31 +0200

    MSS-184

---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-profile-details-form.tpl.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display culturefeed uitpas profile details form.
+ */
+
+?>
+
+<div class="details-form">
+
+  <div class="clearfix form-row">
+    <?php print $first_name; ?>
+    <?php print $last_name; ?>
+  </div>
+  <div class="clearfix form-row">
+    <?php print $dob; ?>
+    <?php print $pob; ?>
+  </div>
+  <div class="clearfix form-row">
+    <?php print $gender; ?>
+    <?php print $nationality; ?>
+  </div>
+  <div class="clearfix form-row">
+    <?php print $street; ?>
+    <?php print $nr; ?>
+  </div>
+  <div class="clearfix form-row">
+    <?php print $zip; ?>
+    <?php print $city; ?>
+  </div>
+  <div class="clearfix form-row">
+    <?php print $tel; ?>
+    <?php print $mobile; ?>
+  </div>
+  <div class="clearfix form-row">
+    <?php print $main_form; ?>
+  </div>
+  <div class="clearfix form-row">
+    <?php print $actions; ?>
+  </div>
+
+</div>
