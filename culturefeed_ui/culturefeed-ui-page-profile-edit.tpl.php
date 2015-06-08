7dc8ee14af292d519a25898c3d54a91c614cebec
HnLn <hans.langouche@gmail.com>
2015-06-08 15:48:19 +0200

    MSS-176 Make form seperate from page callback and add profile menu

---
+++ culturefeed_ui/culturefeed-ui-page-profile-edit.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Template to render the page account edit page.
+ */
+?>
+
+<?php if ($intro): ?>
+<div id="profile-edit-intro">
+  <?php print $intro; ?>
+</div>
+<?php endif; ?>
+
+<?php print $profile_menu; ?>
+
+<div id="profile-edit-form">
+  <?php print $form ?>
+</div>
