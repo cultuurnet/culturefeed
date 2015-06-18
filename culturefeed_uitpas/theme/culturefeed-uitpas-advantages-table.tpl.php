e0f23a8b398f2facb8b7981523c57ad22bde50bc
Julie Vanopdenbosch <julievanopdenbosch@Julies-MacBook-Air.local>
2015-06-18 17:02:22 +0200

    MSS-181 julie aanpassingen markup

--- culturefeed_uitpas/theme/culturefeed-uitpas-advantages-table.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-advantages-table.tpl.php
@@ -1,28 +1,30 @@
-<section class="advantages-overview">
+<section class="overview overview-advantages">
   <?php foreach($items as $item): ?>
-  <article class="<?php print implode(' ', $item['classes']); ?>">
-    <main>
-      <figure class="thumbnail">
-        <?php print $item['image']; ?>
-      </figure>
-      <div class="content">
-        <span class="provider <?php print $item['cardsystem']['class']; ?>"><?php print $item['cardsystem']['name']; ?></span>
-        <h2 class="title"><?php print $item['title']; ?></h2>
-        <ul class="locations">
-          <?php foreach($item['counters'] as $counter): ?>
-          <li class="<?php print $counter['class']; ?>"><?php print $counter['name']; ?></li>
-          <?php endforeach; ?>
-        </ul>
-        <span class="availability"><?php print $item['availability']; ?></span>
-      </div>
-    </main>
-    <aside class="points <?php print $item['points']['classes']; ?>">
-      <span class="points-value"><?php print $item['points']['value']; ?></span>
-      <?php if ($item['points']['remark']): ?>
-        <span class="points-remark"><?php print $item['points']['remark']; ?></span>
-      <?php endif; ?>
-    </aside>
-  </article>
+  <article class="<?php print implode(' ', $item['classes']); ?> clearfix">
+    <div class="row">
+      <div class="main col-md-8 col-lg-9" role="main">
+        <figure class="thumbnail">
+          <?php print $item['image']; ?>
+        </figure>
+        <div class="content">
+          <span class="provider <?php print $item['cardsystem']['class']; ?>"><?php print $item['cardsystem']['name']; ?></span>
+          <h2 class="title"><?php print $item['title']; ?></h2>
+          <ul class="locations">
+            <?php foreach($item['counters'] as $counter): ?>
+            <li class="<?php print $counter['class']; ?>"><?php print $counter['name']; ?></li>
+            <?php endforeach; ?>
+          </ul>
+          <span class="availability"><?php print $item['availability']; ?></span>
+        </div>
+      </div> <!--/ end .main -->
+      <aside class="points <?php print $item['points']['classes']; ?> col-md-4 col-lg-3">
+        <span class="points-value"><?php print $item['points']['value']; ?></span>
+        <?php if ($item['points']['remark']): ?>
+          <span class="points-remark"><?php print $item['points']['remark']; ?></span>
+        <?php endif; ?>
+      </aside> <!--/ end aside -->
+    </div>
+  </article> <!--/ end article -->
   <?php endforeach; ?>
 </section>
 <?php print $pager; ?>
