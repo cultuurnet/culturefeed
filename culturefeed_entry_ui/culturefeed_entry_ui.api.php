7c2c02b6e06b2a088c7c593521b4dde0193e2033
HnLn <hans@2dotstwice.be>
2015-08-24 12:24:05 +0200

    Add api documentation for hook_culturefeed_entry_ui_event_pre_save_alter() .

---
+++ culturefeed_entry_ui/culturefeed_entry_ui.api.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Change the event before making the api call.
+ *
+ * @param CultureFeed_Cdb_Item_Event $event
+ *   The event.
+ * @param array $form
+ *   The form.
+ * @param array $form_state
+ *   The form state.
+ */
+function hook_culturefeed_entry_ui_event_pre_save_alter(CultureFeed_Cdb_Item_Event &$event, array $form, array $form_state) {
+}
