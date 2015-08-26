3d2ae85d3f64274dcd04a569a95507f855213e1b
HnLn <hans@2dotstwice.be>
2015-08-26 11:43:34 +0200

    Add post save hook for entry ui

--- culturefeed_entry_ui/culturefeed_entry_ui.api.php
+++ culturefeed_entry_ui/culturefeed_entry_ui.api.php
@@ -12,3 +12,16 @@
  */
 function hook_culturefeed_entry_ui_event_pre_save_alter(CultureFeed_Cdb_Item_Event &$event, array $form, array $form_state) {
 }
+
+/**
+ * Change the event before making the api call.
+ *
+ * @param CultureFeed_Cdb_Item_Event $event
+ *   The event.
+ * @param array $form
+ *   The form.
+ * @param array $form_state
+ *   The form state.
+ */
+function hook_culturefeed_entry_ui_event_post_save(CultureFeed_Cdb_Item_Event $event, array $form, array $form_state) {
+}
