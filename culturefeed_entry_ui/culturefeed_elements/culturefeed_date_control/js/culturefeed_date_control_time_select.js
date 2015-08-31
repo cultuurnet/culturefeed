cb01ea93abdf07c94e986754f01342b759df2ad4
HnLn <hans@2dotstwice.be>
2015-09-01 00:00:36 +0200

    Ensue time callback is also fired on ajax events.

--- culturefeed_entry_ui/culturefeed_elements/culturefeed_date_control/js/culturefeed_date_control_time_select.js
+++ culturefeed_entry_ui/culturefeed_elements/culturefeed_date_control/js/culturefeed_date_control_time_select.js
@@ -6,17 +6,38 @@
 (function ($) {
 
   Drupal.behaviors.culturefeed_date_control_time_select_complete_time = {
+
     attach: function (context, setings) {
-      $(".date-hour > .time-select-complete-minutes").change(function () {
-        if ($(this).val()) {
-          var minute = $(this).parents(".form-type-date-select").find(".date-minute > .time-select-complete-minutes");
-          if (!minute.val()) {
-            minute.val('00');
+
+      var time_complete = $(".date-hour > .time-select-complete-minutes");
+
+      time_complete.each(function () {
+        var id = $(this).attr('id');
+        if (typeof Drupal.ajax[id] !== 'undefined') {
+          var parentBeforeSerialize = Drupal.ajax[id].options.beforeSerialize;
+          Drupal.ajax[id].options.beforeSerialize = function (element, options) {
+            parentBeforeSerialize(element, options);
+            culturefeed_date_control_time_select_complete_time($('#' + id));
           }
         }
       });
+
+      time_complete.change(function () {
+        if ($(this).val()) {
+          culturefeed_date_control_time_select_complete_time($(this));
+        }
+      });
+
     }
+
+  }
+
+  function culturefeed_date_control_time_select_complete_time(element) {
+    var minute = element.parents(".form-type-date-select").find(".date-minute > .time-select-complete-minutes");
+    if (!minute.val()) {
+      minute.val('00');
+    }
+
   }
 
 })(jQuery);
-
