8266534de16ca8aaf01b380506575240d36018f2
Lieven Goudeseune <lieven.goudeseune@wunderkraut.com>
2015-09-03 13:36:21 +0200

    CUL-4: Don't show parent suggestion on empty search, move html to template.

---
+++ culturefeed_search_ui/theme/culturefeed-search-parent-region-suggestion.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @vars
+ *   - $suggestions (key = search string, value = search url)
+ */
+?>
+<p>
+  <strong><?php print t('Did you mean'); ?></strong>
+  <a class="parent-region-suggestion-link" href="<?php print $parent_region_suggestion_url; ?>"><?php print $parent_region_suggestion_words ?></a>
+</p>
