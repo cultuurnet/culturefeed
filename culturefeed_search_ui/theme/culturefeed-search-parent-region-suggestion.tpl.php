5d1f3f9c31362248fe008a9dcd2f0e1391233535
zuuperman <nils.destoop@gmail.com>
2015-09-04 15:22:10 +0200

    CUL-4: Code review

--- culturefeed_search_ui/theme/culturefeed-search-parent-region-suggestion.tpl.php
+++ culturefeed_search_ui/theme/culturefeed-search-parent-region-suggestion.tpl.php
@@ -6,5 +6,7 @@
 ?>
 <p>
   <strong><?php print t('Did you mean'); ?></strong>
-  <a class="parent-region-suggestion-link" href="<?php print $parent_region_suggestion_url; ?>"><?php print $parent_region_suggestion_words ?></a>
+  <a class="parent-region-suggestion-link" href="<?php print $parent_region_suggestion_url; ?>">
+  <?php print $parent_region_suggestion ?> (+  <?php print t('boroughs') ?>)
+  </a>
 </p>
