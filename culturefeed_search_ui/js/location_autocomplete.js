fdd09737caeeaa0d3cc42d7c8b5545bf2aa708dc
zuuperman <nils.destoop@gmail.com>
2015-09-30 10:59:59 +0200

    CUL-7: Added validation in the change event: Check if the entered value is inside the last autocomplete result. If not: take the first autocomplete value

--- culturefeed_search_ui/js/location_autocomplete.js
+++ culturefeed_search_ui/js/location_autocomplete.js
@@ -27,12 +27,7 @@
 
         },
         change: function (event, ui) {
-          if (Drupal.CulturefeedSearch.lastLocationData.length === 0) {
-            $(this).val('');
-          }
-          else {
-            $(this).val(ui.item.suggestion);
-          }
+          Drupal.CulturefeedSearch.validateSelectedSuggestion(this);
         },
         autoFocus: true,
         autoSelect: true,
@@ -42,32 +37,10 @@
       $("#edit-where").once('location-search-init').categorisedAutocomplete({
         source: Drupal.CulturefeedSearch.getLocationData,
         select: function(event, ui) {
-
-          // Actors are direct links.
-          if (ui.item.type == 'actor') {
-            // Go to the actor page aftoer selecting an actor.
-            window.location.href = ui.item.suggestion;
-            // Disable form submission after selection of an actor item.
-            $(this).parents('form').find('button').attr('disabled', true);
-          }
-          // Trigger location search.
-          else {
-            if (ui.item.value.indexOf(' (+ ') != -1) {
-              // Reset value and label to remove the boroughs text.
-              ui.item.value = ui.item.suggestion;
-              ui.item.label = ui.item.suggestion;
-            }
-            $(this).val(ui.item.suggestion);
-          }
-
+          Drupal.CulturefeedSearch.handleSuggestionSelect(this, ui.item);
         },
         change: function (event, ui) {
-          if (Drupal.CulturefeedSearch.lastLocationData.length === 0) {
-            $(this).val('');
-          }
-          else {
-            $(this).val(ui.item.suggestion);
-          }
+          Drupal.CulturefeedSearch.validateSelectedSuggestion(this);
         },
         autoFocus: true,
         autoSelect: true,
@@ -95,6 +68,7 @@
               if (!(element.label.match(/^Provinc|Regio+/))) {
                 element.label += ' (+ ' + Drupal.t('boroughs') + ')';
               }
+              element.value = element.label;
             }
           });
         }
@@ -106,6 +80,60 @@
     });
   }
 
+  /**
+   * Handle the suggestion select.
+   */
+  Drupal.CulturefeedSearch.handleSuggestionSelect = function(element, selected_suggestion) {
+
+    // Actors are direct links.
+    if (selected_suggestion.type == 'actor') {
+      // Go to the actor page aftoer selecting an actor.
+      window.location.href = selected_suggestion.suggestion;
+      // Disable form submission after selection of an actor item.
+      $(this).parents('form').find('button').attr('disabled', true);
+    }
+    // Trigger location search.
+    else {
+      if (selected_suggestion.value.indexOf(' (+ ') != -1) {
+        // Reset value and label to remove the boroughs text.
+        selected_suggestion.value = selected_suggestion.suggestion;
+        selected_suggestion.label = selected_suggestion.suggestion;
+      }
+      $(element).val(selected_suggestion.suggestion);
+    }
+
+  }
+
+  /**
+   * Validate if the selected suggestion is correct.
+   * If not, take the first correct suggestion.
+   */
+  Drupal.CulturefeedSearch.validateSelectedSuggestion = function(element) {
+
+    var $element = $(element);
+    var current_val = $element.val();
+    var valid_suggestion = false;
+    // Loop over all found location data and check if it matches current value.
+    for (var i = 0; i < Drupal.CulturefeedSearch.lastLocationData.length; i++) {
+      if (Drupal.CulturefeedSearch.lastLocationData[i].label == current_val) {
+        valid_suggestion = true;
+        // Trigger the select handler incase a special case was selected.
+        Drupal.CulturefeedSearch.handleSuggestionSelect(element, Drupal.CulturefeedSearch.lastLocationData[i]);
+        break;
+      }
+    }
+
+    // None of the items in the autocomplete are selected.
+    if (!valid_suggestion) {
+      if (Drupal.CulturefeedSearch.lastLocationData.length === 0) {
+        $element.val('');
+      }
+      else {
+        $element.val(Drupal.CulturefeedSearch.lastLocationData[0].suggestion);
+      }
+    }
+
+  }
 
 
 })(jQuery);
