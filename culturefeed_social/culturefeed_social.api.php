fa9d9dd405e69c7a17cfd4a9795a3229e1a9b5a0
HnLn <hans@2dotstwice.be>
2015-10-08 14:04:51 +0200

    FTIDAG-77 add alter hook to social activity link.

---
+++ culturefeed_social/culturefeed_social.api.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Change the social activity link before rendering it.
+ *
+ * @param array $build
+ *   The build.
+ * @param string $activity_type
+ *   The activity type.
+ * @param CultuurNet\Search\ActivityStatsExtendedEntity $extended_entity
+ *   The extended entity.
+ */
+function hook_culturefeed_social_activity_link(array &$build, $activity_type, ActivityStatsExtendedEntity $extended_entity) {
+}
