9dd9b8533b1793c246f4b2920ceafe5eef49366d
HnLn <hans@2dotstwice.be>
2015-10-28 11:16:13 +0100

    MSS-180

--- culturefeed_uitpas/theme/culturefeed-uitpas-profile-advantages.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-profile-advantages.tpl.php
@@ -11,12 +11,6 @@
  * - $advantages_table: The list of advantages.
  */
 ?>
-<div id="profile_advantages_link">
-  <ul>
-    <li><a href="/culturefeed/profile/uitpas/promotions"><?php print t('Promotions') ?></li>
-    <li><a href="/culturefeed/profile/uitpas/advantages"><?php print t('Welcome Advantages') ?></li>
-  </ul>
-</div>
 
 <div class="profile_advantages">
   <div class="advantages"><?php print $advantages_table; ?></div>
