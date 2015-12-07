1872c3b3d01177e3cc55f6c6822ae5e211c566ec
Reinout Bossuyt <reinout@cultuurnet.be>
2015-12-07 14:36:36 +0100

    UIV-1466 Different actions and messages for non-passholders

--- culturefeed_uitpas/theme/culturefeed-uitpas-profile-section-register.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-profile-section-register.tpl.php
@@ -7,19 +7,34 @@
 
 ?>
 
-<p>
-  <?php echo t("Holders of an UiTPAS can earn points by participating in leisure activities and exchange them for benefits."); ?>
-</p>
+<?php if(culturefeed_uitpas_not_yet_registered()): ?>
 
-<ul class="links">
-  <li><a href="/register_where"><?php echo t("Find out where to get an UiTPAS"); ?></a></li>
-  <li><a href="/promotions"><?php echo t("Promotions"); ?></a></li>
-  <li>
-    <a href="/authenticated?destination=register_uitpas">
-      <strong>
-      <?php echo t("Already an UiTPAS?"); ?>
-      </strong><br/>
-      <?php echo t("Register your UiTPAS online"); ?>
+  <h5><?php echo t("You did not register your UiTPAS yet."); ?></h5>
+
+  <p><?php echo t("Register and..."); ?></p>
+
+  <ul>
+  <li><?php echo t("consult your benefits") ?></li>
+  <li><?php echo t("check points balance") ?></li>
+  <li><?php echo t("access your information") ?></li>
+  </ul>
+
+  <p>
+    <a href="/authenticated?destination=register_uitpas" class="btn btn-primary">
+    <?php echo t("Register your UiTPAS") ?>
     </a>
-  </li>
-</ul>
+  </p>
+
+<?php else: ?>
+
+  <p>
+    <?php echo t("Holders of an UiTPAS can earn points by participating in leisure activities and exchange them for ") . l(t("benefits."),"/promotions"); ?>
+  </p>
+
+  <p>
+    <a href="/register_where" class="btn btn-default">
+      <?php echo t("Find out where to get an UiTPAS"); ?>
+    </a>
+  </p>
+
+<?php endif; ?>
