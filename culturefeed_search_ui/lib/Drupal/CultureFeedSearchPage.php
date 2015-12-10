1ffde8ede7ce376dcf6cb8804508f3caad037c1f
zuuperman <nils.destoop@gmail.com>
2015-12-10 16:42:47 +0100

    CUL-19: Rename variable to better name

--- culturefeed_search_ui/lib/Drupal/CultureFeedSearchPage.php
+++ culturefeed_search_ui/lib/Drupal/CultureFeedSearchPage.php
@@ -225,18 +225,18 @@ class CultureFeedSearchPage {
     $term = str_replace(' AND ', ' ', $term);
 
     $query_parts = explode(' OR ', $term);
-    array_walk($query_parts, function(&$part) {
+    array_walk($query_parts, function(&$search_string) {
 
       // Strip of words between quotes. The spaces don't need to be replaced to AND for them.
-      preg_match_all('/".*?"/', $part, $matches);
+      preg_match_all('/".*?"/', $search_string, $matches);
       foreach ($matches[0] as $match) {
-        $part = str_replace($match, '', $part);
+        $search_string = str_replace($match, '', $search_string);
       }
 
-      $part = str_replace('  ', ' ', $part);
+      $search_string = str_replace('  ', ' ', $search_string);
 
       // Put words with a special character between quotes.
-      $words = explode(' ', trim($part));
+      $words = explode(' ', trim($search_string));
       $parts = array();
       $special_characters = '-';
       foreach ($words as $word) {
@@ -247,15 +247,15 @@ class CultureFeedSearchPage {
       }
 
       // Replace spaces between multiple search words by 'AND'.
-      $part = implode(' AND ', $parts);
+      $search_string = implode(' AND ', $parts);
 
       // Add back the words between quotes.
       if (!empty($matches[0])) {
-        if (empty($part)) {
-          $part .= implode(' AND ', $matches[0]);
+        if (empty($search_string)) {
+          $search_string .= implode(' AND ', $matches[0]);
         }
         else {
-          $part .= ' AND ' . implode(' AND ', $matches[0]);
+          $search_string .= ' AND ' . implode(' AND ', $matches[0]);
         }
       }
 
