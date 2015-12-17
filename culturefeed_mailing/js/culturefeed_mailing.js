71e0a4fda4a6d0f9a6f54c0d51a636c76306a7d7
zuuperman <nils.destoop@gmail.com>
2015-12-17 15:52:41 +0100

    Culturefeed mailing: coding standards cleanup

--- culturefeed_mailing/js/culturefeed_mailing.js
+++ culturefeed_mailing/js/culturefeed_mailing.js
@@ -14,12 +14,12 @@
       // If a userlocation is known, set the default zip value.
       var userLocationJson = $.cookie('Drupal.visitor.uitid.userLocation');
       if (typeof userLocationJson !== 'undefined') {
-      	var userLocation = jQuery.parseJSON(userLocationJson);
-      	if ($('.zip-field').length && userLocation.hasOwnProperty('postal')) {
+        var userLocation = jQuery.parseJSON(userLocationJson);
+        if ($('.zip-field').length && userLocation.hasOwnProperty('postal')) {
           $('.zip-field').val(userLocation.postal);
-      	}
+        }
       }
-      
+
     }
   };
 
