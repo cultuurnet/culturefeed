4f771c487db1d52c3a571c756c3f276f69150f4d
zuuperman <nils.destoop@gmail.com>
2015-12-17 15:34:21 +0100

    CUL-24: Validation

--- culturefeed_mailing/js/culturefeed_mailing.js
+++ culturefeed_mailing/js/culturefeed_mailing.js
@@ -1,13 +1,25 @@
+/**
+ * @file
+ * Js behavior for the mailing forms.
+ */
+
 (function($) {
+
+  /**
+   * Attach the behavior.
+   */
   Drupal.behaviors.culturefeed_mailing = {
     attach: function (context, settings) {
-    	var userLocationJson = $.cookie('Drupal.visitor.uitid.userLocation');
-      if(typeof userLocationJson !== 'undefined') {
+
+      // If a userlocation is known, set the default zip value.
+      var userLocationJson = $.cookie('Drupal.visitor.uitid.userLocation');
+      if (typeof userLocationJson !== 'undefined') {
       	var userLocation = jQuery.parseJSON(userLocationJson);
       	if ($('.zip-field').length && userLocation.hasOwnProperty('postal')) {
-      		$('.zip-field').val(userLocation.postal);
+          $('.zip-field').val(userLocation.postal);
       	}
       }
+      
     }
   };
 
