e351d3ea662411dc07e3eea99da7973827222795
Luk Dens <luk.dens@cultuurnet.be>
2016-04-23 20:10:10 +0200

    UIV-1747 add rel nofollow to facet links that have active items in same facet

--- culturefeed_search_ui/theme/culturefeed-search-facet-item.tpl.php
+++ culturefeed_search_ui/theme/culturefeed-search-facet-item.tpl.php
@@ -9,10 +9,12 @@
  * @var integer $count
  * @var string $url
  * @var boolean $active
+ * @var boolean $active_subitem
  */
 ?>
+
 <?php if ($active): ?>
 <span class="facet-label active"><?php print check_plain($label); ?><span class="facet-count">(<?php print $count; ?>)</span><a href="<?php print $url; ?>" class="facet-remove" title="<?php print t('Remove filter'); ?>">&times;</a></span>
 <?php else: ?>
-<span class="facet-label"><a href="<?php print $url; ?>"><?php print check_plain($label); ?></a></span><span class="facet-count">(<?php print $count; ?>)</span>
+<span class="facet-label"><a href="<?php print $url; ?>" <?php $active_subitem ? print 'rel="nofollow"' : ''; ?>><?php print check_plain($label); ?></a></span><span class="facet-count">(<?php print $count; ?>)</span>
 <?php endif; ?>
