ff44d7ba30fcdfdeed4fc96bc86e18b7c9e5d4ff
Stijn Swaanen <swaanenstijn@gmail.com>
2017-06-02 12:06:43 +0200

    UPS-2269 styling for coupons

--- culturefeed_uitpas/theme/culturefeed-uitpas-profile-coupons.tpl.php
+++ culturefeed_uitpas/theme/culturefeed-uitpas-profile-coupons.tpl.php
@@ -10,18 +10,18 @@
  * - $actions_table: The list of actions.
  */
 ?>
-<div class="profile_coupons">
+<div class="profile-coupons">
   <?php foreach ($coupons as $coupon): ?>
     <div class="coupon-detail">
         <h3><?php print $coupon['name']; ?></h3>
         <?php if (!empty($coupon['description'])): ?>
-            <?php print $coupon['description']; ?><br />
+            <span class="coupon-detail-description"><?php print $coupon['description']; ?></span>
         <?php endif; ?>
         <?php if(isset($coupon['validTo'])): ?>
-            Geldig tot: <?php print $coupon['validTo']; ?><br />
+            <span class="coupon-detail-validto"><strong>Geldig tot:</strong> <?php print $coupon['validTo']; ?></span>
         <?php endif; ?>
         <?php if(isset($coupon['remaining'])): ?>
-            <?php print $coupon['remaining']; ?>
+            <span class="coupon-detail-remaining"><strong>Beschikbaar:</strong> <?php print $coupon['remaining']; ?></span>
         <?php endif; ?>
     </div>
     <hr />
