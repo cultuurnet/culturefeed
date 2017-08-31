fb565f03b139e8e695bc1bfe6b301b8013cd3488
Luk Dens <luk.dens@cultuurnet.be>
2017-08-31 15:29:32 +0200

    WKS-257 use different copy for different states in newsletter subscribe block

--- culturefeed_mailing/theme/culturefeed-mailing-content-block-already-subscribed.tpl.php
+++ culturefeed_mailing/theme/culturefeed-mailing-content-block-already-subscribed.tpl.php
@@ -5,5 +5,5 @@
  */
 ?>
 
-<div class='title'><?php print $title; ?></div>
-<div class='message'><?php print $message; ?></div>
+<div class="message"><?php print $message; ?></div>
+<div class="unsubscribe-link"><?php print $unsubscribe_link; ?></div>
