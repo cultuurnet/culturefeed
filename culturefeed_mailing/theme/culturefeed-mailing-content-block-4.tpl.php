13f2c72c73201f6ce6cbfa020cde72e4c8b5ccc4
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2018-01-02 17:01:38 +0100

    Revert "Merge branch 'develop' into feature/WKS-332"
    This reverts commit be89af5ec4b1bd12b9f6f2638d96782acebc84c5, reversing
    changes made to 3de1927f7eb3b37d39a309edc604af0b736e9542.

---
+++ culturefeed_mailing/theme/culturefeed-mailing-content-block-4.tpl.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ * Template for 4th block in mailing.
+ */
+?>
+
+<tr>
+  <td valign="top">
+    <table border="0" cellpadding="0" cellspacing="0">
+      <tbody>
+        <tr>
+          <td valign="top" width="115">
+            <a href="<?php print $url_image; ?>" title="<?php print $title; ?>"><?php print $image; ?></a>
+          </td>
+          <td valign="top" style="text-align:left;">
+            <a href="<?php print $url_title; ?>" style="color: #000001; font-family: Arial, sans-serif; font-size: 18px; font-weight: bold; line-height: 120%; text-align: left; text-decoration:none;">
+              <?php print $title; ?>
+            </a>
+            <br>
+            <span style="color: #000000; font-family: Arial, sans-serif; font-size: 13px; font-weight: normal; line-height: 120%; text-align: left;">
+              <?php print $intro; ?>
+            </span>
+            <br>
+            <a href="<?php print $url_readon; ?>" style="color: #F10B0D; font-family: Arial, sans-serif; font-size: 13px; font-weight: normal; line-height: 200%; text-decoration: none; border-bottom: 1px dotted #F10B0D;">
+              <?php print $link_title; ?>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </td>
+</tr>
+<tr>
+    <td height="10" style="LINE-HEIGHT: 1px; FONT-SIZE: 1px;"><?php print $spacer; ?>
+    </td>
+</tr>
+<tr>
+    <td bgcolor="#efeeec" height="1" width="600"></td>
+</tr>
+<tr>
+    <td height="10" style="LINE-HEIGHT: 1px; FONT-SIZE: 1px;"><?php print $spacer; ?>
+    </td>
+</tr>
