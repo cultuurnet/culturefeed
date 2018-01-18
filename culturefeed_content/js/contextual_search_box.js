63900a420e87936e8446318302705da0b93b7f81
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2018-01-18 11:23:38 +0100

    Fix toggling issue, block enter key presses on postal code textfield

--- culturefeed_content/js/contextual_search_box.js
+++ culturefeed_content/js/contextual_search_box.js
@@ -4,7 +4,14 @@
     attach: function (context, settings) {
       $('#toggle_postal_code').click(function(e){
         e.preventDefault();
-        $('div.postal-code-container').toggle();
+        $('div.postal-code-container').show();
+      });
+
+      $('#edit-postal-code').keypress(function(e){
+        if (e.which == 13) {
+          e.preventDefault();
+          $('div.postal-code-container').hide();
+        }
       });
     }
   }
