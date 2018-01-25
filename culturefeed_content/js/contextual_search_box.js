e2f7eeafa032e1b8bbaaec8dbb0bc3a32bafaa24
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2018-01-25 16:20:42 +0100

    Fix issues with multiple rendered contextual search boxes

--- culturefeed_content/js/contextual_search_box.js
+++ culturefeed_content/js/contextual_search_box.js
@@ -2,15 +2,17 @@
 
   Drupal.behaviors.togglePostalCodeField = {
     attach: function (context, settings) {
-      $('#toggle_postal_code').click(function(e){
+      $('.toggle_postal_code').click(function(e){
         e.preventDefault();
-        $('div.postal-code-container').show();
+        var id = '#postal_code_container_' + $(this).data('id');
+        $(id).show();
       });
 
-      $('#edit-postal-code').keypress(function(e){
+      $('.postal-code-container-postal-code').keypress(function(e){
         if (e.which == 13) {
           e.preventDefault();
-          $('div.postal-code-container').hide();
+          var id = '#postal_code_container_' + $(this).data('id');
+          $(id).hide();
         }
       });
     }
