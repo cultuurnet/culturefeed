5c8c9fd459f6d02dcbcabed0fb5594ebdfdfdf8c
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2018-01-26 11:16:22 +0100

    Change link order for consistency

--- culturefeed_mailing/theme/culturefeed-mailing-content-block-already-subscribed.tpl.php
+++ culturefeed_mailing/theme/culturefeed-mailing-content-block-already-subscribed.tpl.php
@@ -6,5 +6,5 @@
 ?>
 
 <div class="message"><?php print $message; ?></div>
-<div class="update-link"><?php print $update_link; ?></div>
 <div class="unsubscribe-link"><?php print $unsubscribe_link; ?></div>
+<div class="update-link"><?php print $update_link; ?></div>
