799c6d11e18b40b74dcd7f4635eab8c792b65ecd
Nils Destoop <nils.destoop@gmail.com>
2018-03-27 18:18:55 +0200

    Review of WKS-319

--- culturefeed_content/js/contextual_search_box.js
+++ culturefeed_content/js/contextual_search_box.js
@@ -2,18 +2,9 @@
 
   Drupal.behaviors.togglePostalCodeField = {
     attach: function (context, settings) {
-      $('.toggle_postal_code').click(function(e){
+      $('.toggle_postal_code').once('toggle-init').click(function(e){
         e.preventDefault();
-        var id = '#postal_code_container_' + $(this).data('id');
-        $(id).show();
-      });
-
-      $('.postal-code-container-postal-code').keypress(function(e){
-        if (e.which == 13) {
-          e.preventDefault();
-          var id = '#postal_code_container_' + $(this).data('id');
-          $(id).hide();
-        }
+        $('#' + $(this).data('id')).toggle();
       });
     }
   }
---
+++ culturefeed_content/theme/culturefeed-content-result-list.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Provides the template to render the results of a culturefeed content field.
+ */
+?>
+
+<h2><?php print $title; ?></h2>
+
+<?php if (empty($items)): ?>
+<div><?php print t('No results found.'); ?></div>
+<?php else: ?>
+<div id="<?php print $wrapper_id ?>">
+  <?php foreach ($items as $item): ?>
+    <?php print $item; ?>
+  <?php endforeach; ?>
+</div>
+
+<a href="<?php print $show_more_url; ?>"><?php print t('Show more'); ?></a>
+<?php endif; ?>
